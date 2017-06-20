89f943b3745cb2c47a3efe94640a65e0aa4e98d7
Dan Gillespie <dan.gillespie@coreos.com>
2017-06-19 17:46:21 -0700

    embed prometheus query/storage engine in API mock

--- pkg/promsum/prometheus_test.go
+++ pkg/promsum/prometheus_test.go
@@ -4,48 +4,55 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/promql"
 )
 
 // mockPromAPI implements the Prometheus API interface.
 var _ v1.API = mockPromAPI{}
 
 // NewMockPromAPI initializes a mock of the Prometheus API.
-func NewMockPromAPI(t *testing.T) mockPromAPI {
+func NewMockPromAPI(t *testing.T, input ...string) mockPromAPI {
+	test, err := promql.NewTest(t, strings.Join(input, "\n"))
+	if err != nil {
+		t.Fatal("Could not setup Prometheus mock: ", err)
+	}
 	return mockPromAPI{
-		responseCh: make(chan mockPromResponse, 10),
-		T:          t,
+		promTest: test,
+		T:        t,
 	}
 }
 
 // mockPromAPI allows Prometheus API responses to be injected for testing purposes.
 type mockPromAPI struct {
-	// responseCh holds values that are returned when API queries are made.
-	responseCh chan mockPromResponse
+	promTest *promql.Test
 	// for test logging
 	*testing.T
 	// counts API calls for use in hashing
 	counter uint32
 }
 
-// mockPromResponse contains both a Value and Error to be returned in mocking.
-type mockPromResponse struct {
-	model.Value
-	error
-}
-
 // QueryRange returns the next mockPromResponse as the response to any query.
 func (a mockPromAPI) QueryRange(ctx context.Context, query string, r v1.Range) (model.Value, error) {
 	id := a.id(query, r.Start, r.End)
 	a.Logf("%s: Mock Prometheus queried with '%s' for %v to %v.", id, query, r.Start.Unix(), r.End.Unix())
-	response := <-a.responseCh
-	a.Logf("%s: Mock Prometheus responded with value='%v', error='%v'", id, response.Value, response.error)
-	return response.Value, response.error
+
+	// perform query
+	start, end := model.TimeFromUnixNano(r.Start.UnixNano()), model.TimeFromUnixNano(r.End.UnixNano())
+	pQuery, err := a.promTest.QueryEngine().NewRangeQuery(query, start, end, r.Step)
+	if err != nil {
+		return nil, err
+	}
+
+	response := pQuery.Exec(ctx)
+	a.Logf("%s: Mock Prometheus responded with value='%v', error='%v'", id, response.Value, response.Err)
+	return response.Value, response.Err
 }
 
 // id returns a unique identifier for the query based on query text and optionally a series of times.
