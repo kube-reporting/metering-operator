79a193e656affcf263059e2b1efcc32c97d55b7e
Dan Gillespie <dan.gillespie@coreos.com>
2017-07-06 11:04:07 -0700

    install.sh: add query layer to setup

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-# Modify a Tectonic install to use the new kube-state-metrics that has node ID working
+#!/bin/bash
 
+# Modify a Tectonic install to use the new kube-state-metrics that has node ID working
 kubectl -n tectonic-system patch deployment kube-state-metrics -p '{"spec":{"template":{"spec":{"containers":[{"name":"kube-state-metrics","image":"quay.io/dan_gillespie/kube-state-metrics:v0.5.0"}]}}}}'
+
+# install query layer (optional for collection)
+kubectl apply -f ./hdfs
+sleep 5
+
+kubectl apply -f ./hive
+kubectl apply -f ./presto
