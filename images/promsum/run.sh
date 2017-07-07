23f2afbc67df67e6500cb50f058155db273c6301
Dan Gillespie <dan.gillespie@coreos.com>
2017-07-07 07:18:32 -0700

    add promsum image

---
+++ images/promsum/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+POLL_INTERVAL=${POLL_INTERVAL:-300}
+
+if [[ -z "${S3_BUCKET:-}" ]]; then
+  echo "The variable S3_BUCKET must be set."
+  exit 1
+fi
+
+if [[ -z "${S3_PATH:-}" ]]; then
+  echo "The variable S3_PATH must be set."
+  exit 1
+fi
+
+if [[ -z "${QUERY:-}" ]]; then
+  echo "The variable QUERY must be set."
+  exit 1
+fi
+
+echo "Logging usage data for cluster..."
+while true; do
+  promsum -path s3:///${S3_BUCKET}/${S3_PATH} -before ${POLL_INTERVAL}s ${QUERY}
+  echo "Waiting ${POLL_INTERVAL} seconds before polling again."
+  sleep ${POLL_INTERVAL}
+done
+
