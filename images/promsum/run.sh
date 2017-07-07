417b4fa7d64486ed566fe8c4e66f7f820711f858
Dan Gillespie <dan.gillespie@coreos.com>
2017-07-07 08:05:57 -0700

    add default subject to promsum image

--- images/promsum/run.sh
+++ images/promsum/run.sh
@@ -2,6 +2,7 @@
 
 POLL_INTERVAL=${POLL_INTERVAL:-300}
 PROM=${PROM:-http://prometheus.tectonic-system:9090}
+SUBJECT=${SUBJECT:-kube-chargeback}
 
 if [[ -z "${S3_BUCKET:-}" ]]; then
   echo "The variable S3_BUCKET must be set."
@@ -20,7 +21,7 @@ fi
 
 echo "Logging usage data for cluster..."
 while true; do
-  promsum -prom ${PROM} -path s3:///${S3_BUCKET}/${S3_PATH} -before ${POLL_INTERVAL}s ${QUERY}
+  promsum -subject ${SUBJECT} -prom ${PROM} -path s3:///${S3_BUCKET}/${S3_PATH} -before ${POLL_INTERVAL}s ${QUERY}
   echo "Waiting ${POLL_INTERVAL} seconds before polling again."
   sleep ${POLL_INTERVAL}
 done
