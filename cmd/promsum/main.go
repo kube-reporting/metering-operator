15556c4cf28b8f2dabdbac6abee4902271439dda
Dan Gillespie <dan.gillespie@coreos.com>
2017-08-01 05:57:53 -0700

    cmd/promsum: add flag for record rollup

--- cmd/promsum/main.go
+++ cmd/promsum/main.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	cb "github.com/coreos-inc/kube-chargeback/pkg/chargeback"
-	"github.com/coreos-inc/kube-chargeback/pkg/promsum"
 
 	"github.com/prometheus/client_golang/api"
 )
@@ -16,6 +15,7 @@ var (
 	before        time.Duration
 	timePrecision time.Duration
 	window        time.Duration
+	rollup        time.Duration
 	subject       string
 	storeURL      string
 	aggregate     bool
@@ -25,7 +25,8 @@ var (
 func init() {
 	flag.DurationVar(&before, "before", 1*time.Hour, "duration before present to start collect billing data")
 	flag.DurationVar(&timePrecision, "precision", time.Second, "unit of time used for stored amounts")
-	flag.DurationVar(&window, "window", 5*time.Minute, "interval Prometheus should return query results")
+	flag.DurationVar(&window, "window", 20*time.Second, "interval Prometheus should return query results")
+	flag.DurationVar(&rollup, "rollup", 10*time.Minute, "interval at which billing records are bundled into a single record")
 	flag.StringVar(&subject, "subject", fmt.Sprintf("%x", time.Now().Nanosecond()), "name used to group billing data")
 	flag.StringVar(&storeURL, "path", "file://data", "URL to the location that records should be stored")
 	flag.StringVar(&promURL, "prom", "http://localhost:9090", "URL of the Prometheus to be queried")
@@ -57,41 +58,15 @@ func main() {
 		log.Fatal("could not setup remote: ", err)
 	}
 
-	log.Println("Testing metering...")
-	records, err := promsum.Meter(prom, query, subject, billingRng, timePrecision, window)
-	if err != nil {
-		log.Fatalf("Failed to meter for %v for query '%s': %v", billingRng, query, err)
-	}
-
-	var total float64
-	fmt.Println("Produced records:")
-	for _, r := range records {
-		log.Println("- ", r)
-		total += r.Amount
-	}
-	fmt.Printf("Total usage over %v: %f\n", billingRng, total)
-
 	log.Println("Testing storage...")
 	store, err := setupStore(storeURL)
 	if err != nil {
 		log.Fatal("Could not setup storage: ", err)
 	}
 
-	records, err = bill(prom, store, query, subject, billingRng, timePrecision, window)
+	_, err = bill(prom, store, query, subject, billingRng, timePrecision, window, rollup)
 	if err != nil {
 		log.Fatalf("Failed to bill for period %v to %v for query '%s': %v",
 			billingRng.Start, billingRng.End, query, err)
-	}
-
-	if aggregate {
-		totals, err := promsum.Aggregate(records, billingRng, []string{"pod", "namespace", "container"})
-		if err != nil {
-			log.Fatalf("Failed to aggregate for %s over %v: %v", query, billingRng, err)
-		}
-
-		log.Println("Found aggregates:")
-		for _, r := range totals {
-			log.Println(r.String())
-		}
 	}
 }
