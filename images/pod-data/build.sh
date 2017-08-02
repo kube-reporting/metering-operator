5627aaf83a5136e06b044235a5bbb27af80c589d
Dan Gillespie <dan.gillespie@coreos.com>
2017-08-02 07:46:43 -0700

    images/hive: set report output file extension to '.csv.gz'

--- images/pod-data/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Builds image for hive
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-IMAGE_NAME=${@:-"quay.io/fest-data-demo/pod-data:0.2"}
-
-docker build -t "${IMAGE_NAME}" ${DIR}
