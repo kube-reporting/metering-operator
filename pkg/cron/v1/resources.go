80f2914d5a4f2500733de840a9dc911b144ef63d
Dan Gillespie <dan.gillespie@coreos.com>
2017-08-01 21:29:51 -0700

    pkg/cron/v1: client & type boilerplate for Cron

---
+++ pkg/cron/v1/resources.go
@@ -0,0 +1,35 @@
+package v1
+
+import (
+	"fmt"
+
+	extensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	Group   = "cron.coreos.com"
+	Version = "v1"
+)
+
+var (
+	// Resources are the CRDs deployed for Cron.
+	Resources = []extensions.CustomResourceDe***REMOVED***nition{
+		ReportResource,
+	}
+)
+
+var ReportResource = extensions.CustomResourceDe***REMOVED***nition{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: fmt.Sprintf("%s.%s", CronPlural, Group),
+	},
+	Spec: extensions.CustomResourceDe***REMOVED***nitionSpec{
+		Group:   Group,
+		Version: Version,
+		Names: extensions.CustomResourceDe***REMOVED***nitionNames{
+			Plural: CronPlural,
+			Kind:   CronKind,
+		},
+		Scope: extensions.NamespaceScoped,
+	},
+}
