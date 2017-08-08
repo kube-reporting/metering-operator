2e525286331fcaa0a6ff91a6e0af5a0a902e4079
Dan Gillespie <dan.gillespie@coreos.com>
2017-08-08 06:40:32 -0700

    pkg/{hive,presto}: add methods create tables and run pod usage report

--- pkg/hive/result.go
+++ pkg/hive/result.go
@@ -12,10 +12,29 @@ var (
 		"stop timestamp",
 		"labels map<string, string>",
 	}
+
+	PodUsageColumns = []string{
+		"pod string",
+		"namespace string",
+		"node string",
+		"usage double",
+		"begin timestamp",
+		"stop timestamp",
+		"labels map<string, string>",
+	}
 )
 
 // CreatePodCostTable instantiates a new Hive table to hold the result of a Pod/dollar report.
 func CreatePodCostTable(conn *Connection, tableName, bucket, pre***REMOVED***x string) error {
+	return createReportTable(conn, tableName, bucket, pre***REMOVED***x, PodCostColumns)
+}
+
+// CreatePodUsageTable instantiates a table for Pod usage aggregates.
+func CreatePodUsageTable(conn *Connection, tableName, bucket, pre***REMOVED***x string) error {
+	return createReportTable(conn, tableName, bucket, pre***REMOVED***x, PodUsageColumns)
+}
+
+func createReportTable(conn *Connection, tableName, bucket, pre***REMOVED***x string, columns []string) error {
 	if conn == nil {
 		return errors.New("connection to Hive cannot be nil")
 	} ***REMOVED*** if conn.session == nil {
@@ -24,6 +43,6 @@ func CreatePodCostTable(conn *Connection, tableName, bucket, pre***REMOVED***x s
 
 	// use s3n HDFS driver for s3
 	location := s3nLocation(bucket, pre***REMOVED***x)
-	query := createTable(tableName, location, AWSUsageSerde, AWSUsageSerdeProps, PodCostColumns, false)
+	query := createTable(tableName, location, AWSUsageSerde, AWSUsageSerdeProps, columns, false)
 	return conn.Query(query)
 }
--- pkg/presto/pods.go
+++ pkg/presto/pods.go
@@ -11,14 +11,25 @@ import (
 	cb "github.com/coreos-inc/kube-chargeback/pkg/chargeback/v1"
 )
 
+// RunAWSPodUsageReport creates a report with Pod usage.
+func RunAWSPodUsageReport(presto *sql.DB, promsumTable, outTable string, rng cb.Range) error {
+	reportQuery := podUsageQuery(promsumTable, rng.Start, rng.End)
+	return executeInsertQuery(presto, outTable, reportQuery)
+}
+
 // RunAWSPodDollarReport runs Presto queries to create a Pod Cost summary with AWS billing and Kubernetes usage data.
 func RunAWSPodDollarReport(presto *sql.DB, promsumTable, awsTable, outTable string, rng cb.Range) error {
+	reportQuery := podDollarQuery(promsumTable, awsTable, rng.Start, rng.End)
+	return executeInsertQuery(presto, outTable, reportQuery)
+}
+
+// executeInsertQuery performs the query an INSERT into the table target. It's expected target has the correct schema.
+func executeInsertQuery(presto *sql.DB, target, query string) error {
 	if presto == nil {
 		return errors.New("presto instance of DB cannot be nil")
 	}
 
-	reportQuery := podDollarQuery(promsumTable, awsTable, rng.Start, rng.End)
-	insert := fmt.Sprintf("INSERT INTO %s %s", outTable, reportQuery)
+	insert := fmt.Sprintf("INSERT INTO %s %s", target, query)
 	result, err := presto.Query(insert)
 	if err == nil {
 		cols, err := result.Columns()
@@ -28,6 +39,27 @@ func RunAWSPodDollarReport(presto *sql.DB, promsumTable, awsTable, outTable stri
 		fmt.Println(cols)
 	}
 	return err
+}
+
+// podUsageQuery is a Presto query calculating Pod usage based on request.
+func podUsageQuery(promsumTable string, startPeriod, endPeriod time.Time) string {
+	query := `SELECT pod, namespace, node, sum(amount) as usage, min(begin) as begin, max(stop) as stop, labels
+	FROM (
+	    SELECT kubeUsage.subject,
+		kubeUsage.amount as amount,
+		kubeUsage."start" as begin,
+		kubeUsage."end" as stop,
+		kubeUsage.labels as labels,
+		kubeUsage.labels['pod'] as pod,
+		kubeUsage.labels['namespace'] as namespace,
+		date_diff('millisecond', kubeUsage."start", kubeUsage."end") as duration,
+		split_part(split_part(kubeUsage.labels['provider_id'], ':///', 2), '/', 2) as node
+	    FROM %s as kubeUsage
+	    WHERE kubeUsage."start" >= timestamp '%s'
+	    AND kubeUsage."end" <= timestamp '%s'
+	)
+GROUP BY pod, namespace, node, labels`
+	return fmt.Sprintf(query, promsumTable, prestoTime(startPeriod), prestoTime(endPeriod))
 }
 
 // podDollarQuery is a Presto query which calculates Cost Per Pod over a period.
