7b643a94e3d19a273d7d6a18cf281562efd50139
Dan Gillespie <dan.gillespie@coreos.com>
2017-08-08 06:40:32 -0700

    pkg/{hive,presto}: add methods create tables and run pod usage report

--- pkg/hive/result.go
+++ pkg/hive/result.go
@@ -12,10 +12,29 @@ var (
 		"stop timestamp",
 		"labels map<string, string>",
 	}
+
+	PodUsageColumns = []string{
+		"pod string",
+		"namespace string",
+		"node string",
+		"usage double",
+		"begin timestamp",
+		"stop timestamp",
+		"labels map<string, string>",
+	}
 )
 
 // CreatePodCostTable instantiates a new Hive table to hold the result of a Pod/dollar report.
 func CreatePodCostTable(conn *Connection, tableName, bucket, prefix string) error {
+	return createReportTable(conn, tableName, bucket, prefix, PodCostColumns)
+}
+
+// CreatePodUsageTable instantiates a table for Pod usage aggregates.
+func CreatePodUsageTable(conn *Connection, tableName, bucket, prefix string) error {
+	return createReportTable(conn, tableName, bucket, prefix, PodUsageColumns)
+}
+
+func createReportTable(conn *Connection, tableName, bucket, prefix string, columns []string) error {
 	if conn == nil {
 		return errors.New("connection to Hive cannot be nil")
 	} else if conn.session == nil {
@@ -24,6 +43,6 @@ func CreatePodCostTable(conn *Connection, tableName, bucket, prefix string) erro
 
 	// use s3n HDFS driver for s3
 	location := s3nLocation(bucket, prefix)
-	query := createTable(tableName, location, AWSUsageSerde, AWSUsageSerdeProps, PodCostColumns, false)
+	query := createTable(tableName, location, AWSUsageSerde, AWSUsageSerdeProps, columns, false)
 	return conn.Query(query)
 }
