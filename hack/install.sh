363e39a028638e4bc55e4d0b05d44b221c0a0ba4
Dan Gillespie <dan.gillespie@coreos.com>
2017-08-11 12:40:27 -0700

    hack: setup Tectonic pull secret in chargeback ns

--- hack/install.sh
+++ hack/install.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/util.sh
 
 msg "Creating chargeback namespace"
 kube-install manifests/chargeback/namespace.yaml
+
+msg "Configuring pull secrets"
+copy-tectonic-pull
 
 msg "Installing collection layer (with build of kube-state-metrics with Node info)"
 kube-install manifests/kube-state-metrics manifests/promsum manifests/prom-operator
--- hack/uninstall.sh
+++ hack/uninstall.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. ${DIR}/util.sh
+source ${DIR}/util.sh
 
 msg "Removing chargeback namespace"
 kube-remove manifests/chargeback/namespace.yaml
