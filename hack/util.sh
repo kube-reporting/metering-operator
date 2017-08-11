7e45dfd5ddaddcbc7621fe96bc1a81e6fbd95bc6
Dan Gillespie <dan.gillespie@coreos.com>
2017-08-11 12:40:27 -0700

    hack: setup Tectonic pull secret in chargeback ns

--- hack/util.sh
+++ hack/util.sh
@@ -1,5 +1,9 @@
 # This should not be invoked directly. It provides functions and data for other scripts.
 
+CHARGEBACK_NAMESPACE="tectonic-chargeback"
+TECTONIC_NAMESPACE="tectonic-system"
+TECTONIC_PULL_SECRET="coreos-pull-secret"
+
 function kube-install() {
   local ***REMOVED***les=$(kubectl_***REMOVED***les $@)
   kubectl apply ${***REMOVED***les}
@@ -12,6 +16,12 @@ function kube-remove() {
 
 function msg() {
   echo -e "\x1b[1;35m${@}\x1b[0m"
+}
+
+function copy-tectonic-pull() {
+  local pullSecret=$(kubectl --namespace=${TECTONIC_NAMESPACE} get secrets ${TECTONIC_PULL_SECRET} -o json)
+  pullSecret="${pullSecret/${TECTONIC_NAMESPACE}/${CHARGEBACK_NAMESPACE}}"
+  echo ${pullSecret} | kube-install -
 }
 
 # formats flags for kubectl for the given ***REMOVED***les
