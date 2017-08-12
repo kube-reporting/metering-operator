188f2453ccef2f11e9cbf2d30cc1724c85d90601
Dan Gillespie <dan.gillespie@coreos.com>
2017-08-11 17:22:36 -0700

    hack: create AWS secret for user if envvars exist

--- hack/install.sh
+++ hack/install.sh
@@ -5,6 +5,27 @@ source ${DIR}/util.sh
 msg "Creating chargeback namespace"
 kube-install manifests/chargeback/namespace.yaml
 
+accessKey=${AWS_ACCESS_KEY_ID-"<base64 encoded AWS_ACCESS_KEY_ID>"}
+accessSecret=${AWS_SECRET_ACCESS_KEY-"<base64 encoded AWS_SECRET_ACCESS_KEY>"}
+setupAWS=n
+if [[ $accessKey != \<b* ]] && [[ $accessKey != \<b* ]]; then
+  if [ -t 0 ] && [ -t 1 ]; then
+    read -p "AWS credentials (${accessKey}) detected. Would you like to create a secret for Chargeback using them? [y/N]" setupAWS
+  ***REMOVED***
+  accessKey=$(printf "${accessKey}" | base64)
+  accessSecret=$(printf "${accessSecret}" | base64)
+***REMOVED***
+
+if [[ "${setupAWS}" == "y" ]]; then
+  aws_secret ${accessKey} ${accessSecret} | kube-install -
+***REMOVED***
+  echo "To have chargeback setup AWS credentials for you: set AWS_ACCESS_KEY_ID + AWS_SECRET_ACCESS_KEY, and re-run this script."
+  echo "Alternatively, you can manually create the secret using the manifest below:"
+  echo "-------"
+  aws_secret "${accessKey}" "${accessSecret}"
+  echo "-------"
+***REMOVED***
+
 msg "Con***REMOVED***guring pull secrets"
 copy-tectonic-pull
 
