5af5d17a15add37369c33a4e3f1301c3442373b3
Chance Zibolski <chance.zibolski@coreos.com>
2017-10-16 12:18:23 -0700

    pkg/hive: Add function for adding partitions to existing AWS Billing table

--- pkg/hive/aws.go
+++ pkg/hive/aws.go
@@ -38,3 +38,34 @@ func CreateAWSUsageTable(queryer Queryer, tableName, bucket, prefix string, mani
 	query := createTable(tableName, location, AWSUsageSerde, AWSUsageSerdeProps, columns, awsPartitions, true, true)
 	return queryer.Query(query)
 }
+
+const hiveTimestampLayout = "2006-01-02 15:04:05.000000000"
+
+func UpdateAWSUsageTable(queryer Queryer, tableName, bucket, prefix string, manifests []*aws.Manifest) error {
+	partitionStr := "PARTITION (`billing_period_start`='%s', `billing_period_end`='%s', `assemblyId`='%s') LOCATION '%s'"
+	var stmts []string
+	// A map containing locations we've already added to ensure we do not
+	// attempt to add a partition multiple times, in case the assemblyId shows
+	// up in multiple manifests
+	locations := make(map[string]struct{})
+	for _, manifest := range manifests {
+		for _, manifestPath := range manifest.Paths() {
+			location, err := s3Location(bucket, manifestPath)
+			if err != nil {
+				return err
+			}
+			if _, exists := locations[location]; exists {
+				continue
+			}
+			locations[location] = struct{}{}
+			stmt := fmt.Sprintf(partitionStr, manifest.BillingPeriod.Start.Format(hiveTimestampLayout), manifest.BillingPeriod.End.Format(hiveTimestampLayout), manifest.AssemblyID, location)
+			stmts = append(stmts, stmt)
+		}
+
+	}
+	if len(stmts) == 0 {
+		return nil
+	}
+	query := fmt.Sprintf("ALTER TABLE %s ADD IF NOT EXISTS %s", tableName, strings.Join(stmts, " "))
+	return queryer.Query(query)
+}
