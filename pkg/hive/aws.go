9c1621c672351c3a5fea02e025b773367d0dfd9f
Chance Zibolski <chance.zibolski@coreos.com>
2017-10-16 12:17:25 -0700

    pkg/hive: Support creating tables with partitions
    To be used by AWS Billing datastore, which needs partitions so we can
    point the table at multiple directories, each containing a subset of a
    months billing data.

--- pkg/hive/aws.go
+++ pkg/hive/aws.go
@@ -1,6 +1,9 @@
 package hive
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/coreos-inc/kube-chargeback/pkg/aws"
 )
 
@@ -16,16 +19,22 @@ var (
 		"mapkey.delim":         "undefined",
 		"timestamp.formats":    "yyyy-MM-dd'T'HH:mm:ssZ",
 	}
+
+	awsPartitions = map[string]string{
+		"assemblyId":           "string",
+		"billing_period_start": "timestamp",
+		"billing_period_end":   "timestamp",
+	}
 )
 
 // CreateAWSUsageTable instantiates a new external Hive table for AWS Billing/Usage reports stored in S3.
-func CreateAWSUsageTable(queryer Queryer, tableName, bucket, prefix string, manifest aws.Manifest) error {
+func CreateAWSUsageTable(queryer Queryer, tableName, bucket, prefix string, manifest *aws.Manifest) error {
 	location, err := s3Location(bucket, prefix)
 	if err != nil {
 		return err
 	}
 	columns := manifest.Columns.HQL()
 
-	query := createTable(tableName, location, AWSUsageSerde, AWSUsageSerdeProps, columns, true, true)
+	query := createTable(tableName, location, AWSUsageSerde, AWSUsageSerdeProps, columns, awsPartitions, true, true)
 	return queryer.Query(query)
 }
