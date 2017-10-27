53717c79df25c875a59158b84a8ebaa0b31ab80d
Chance Zibolski <chance.zibolski@coreos.com>
2017-10-27 15:45:19 -0700

    Makefile: Handle concurrent builds better
    The old approach of the codegen_trigger_regenerate_if_changed didn't
    work well when multiple sub-makes were all running concurrently via make
    -j. The problem was that the codegen removes the old files, which
    resulted in all submakes fighting.
    
    The update to the makefile makes it try to regenerate before running go
    build, but still only regenerates the k8s code if something has changed.

--- hack/codegen_output_files.sh
+++ hack/codegen_output_files.sh
@@ -6,18 +6,12 @@
 # ./hack/update-codegen.sh, since we don't necessarily know all the outputted
 # files ahead of time (or rather, it's pain to hardcode the files).
 # If the client has been generated before, this will output the list of files
-# generated which are used as the target, otherwise it prints the date in unix
-# time which will never exist as an output file, resulting in a rebuild
+# generated which are used as the target, otherwise it will print nothing,
+# and you will need to run ./hack/update-codegen.sh manually.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GENERATED_DIR=$(realpath "${DIR}/../pkg/generated")
 OUTPUT=$(find "$GENERATED_DIR" -type f -name '*.go')
 
-# If there's no generated files in the output, then touch all the source files
-# to force Make to re-build the generated files target.
-if [ -z "$OUTPUT" ]; then
-    date +'%s'
-else
-    echo "$OUTPUT"
-fi
+echo "$OUTPUT"
 
