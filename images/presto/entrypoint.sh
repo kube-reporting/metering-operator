374c5cff5422cf1854eb4a0936c576c308401cfb
Chance Zibolski <chance.zibolski@coreos.com>
2017-11-09 17:58:42 -0800

    images/presto: Update presto to 0.188 and use jdk slim image

--- images/presto/entrypoint.sh
+++ images/presto/entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-function addProperty() {
+function addSimpleProperty() {
   local path=$1
   local name=$2
   local value=$3
 
   echo "${name}=${value}" >> ${path}
+}
+
+function addXMLProperty() {
+  local path=$1
+  local name=$2
+  local value=$3
+
+  local entry="<property><name>$name</name><value>${value}</value></property>"
+  local escapedEntry=$(echo $entry | sed 's/\//\\\//g')
+  sed -i "/<\/configuration>/ s/.*/${escapedEntry}\n&/" $path
 }
 
 function configure() {
@@ -21,12 +31,21 @@ function configure() {
         name=`echo ${c} | perl -pe 's/___/-/g; s/__/@/g; s/_/./g; s/@/_/g;'`
         var="${envPrefix}_${c}"
         value=${!var}
+
         echo " - Setting $name=$value"
-        addProperty $path $name "$value"
+        if [[ $path = *.xml ]]; then
+            addXMLProperty $path $name "$value"
+        elif [[ $path = *.properties ]]; then
+            addSimpleProperty $path $name "$value"
+        else
+            echo "unsupported file extension for $file, must end in .properties or .xml"
+        fi
     done
 }
 
-
-configure ${PRESTO_HOME}/etc/catalog/hive.properties hive-catalog HIVE_CATALOG
+# Presto
+configure "${PRESTO_HOME}/etc/catalog/hive.properties" hive-catalog HIVE_CATALOG
+configure "${PRESTO_HOME}/etc/config.properties" presto-conf PRESTO_CONF
 
 exec $@
+
