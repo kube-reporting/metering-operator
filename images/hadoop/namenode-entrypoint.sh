37e9e8c6b569d75385fb50e6af5ae0118b50f932
Chance Zibolski <chance.zibolski@coreos.com>
2017-11-09 17:58:42 -0800

    images/hadoop: Add configurations for HDFS

---
+++ images/hadoop/namenode-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+namedir=/hadoop/dfs/name
+if [ ! -d $namedir ]; then
+  echo "Namenode name directory not found: $namedir"
+  exit 2
+***REMOVED***
+
+if [ -z "$CLUSTER_NAME" ]; then
+  echo "Cluster name not speci***REMOVED***ed"
+  exit 2
+***REMOVED***
+
+if [ "`ls -A $namedir`" == "" ]; then
+  echo "Formatting namenode name directory: $namedir"
+  $HADOOP_PREFIX/bin/hdfs --con***REMOVED***g $HADOOP_CONF_DIR namenode -format $CLUSTER_NAME
+***REMOVED***
+
+$HADOOP_PREFIX/bin/hdfs --con***REMOVED***g $HADOOP_CONF_DIR namenode
