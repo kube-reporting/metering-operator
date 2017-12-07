6ecce2e2ce4dd6ad927a115cfbadef207cd788fa
Chance Zibolski <chance.zibolski@coreos.com>
2017-12-07 12:47:04 -0800

    hive: update comments to indicate promsum tables are not external

--- pkg/hive/promsum.go
+++ pkg/hive/promsum.go
@@ -9,7 +9,8 @@ var (
 	}
 )
 
-// CreatePromsumTable instantiates a new external Hive table for Prometheus observation data stored in S3.
+// CreatePromsumTable instantiates a new Hive table for Prometheus observation
+// data stored in S3.
 func CreatePromsumTable(queryer Queryer, tableName, bucket, prefix string) error {
 	location, err := s3Location(bucket, prefix)
 	if err != nil {
@@ -19,7 +20,7 @@ func CreatePromsumTable(queryer Queryer, tableName, bucket, prefix string) error
 	return queryer.Query(query)
 }
 
-// CreateLocalPromsumTable instantiates a new external Hive table for Prometheus
+// CreateLocalPromsumTable instantiates a new Hive table for Prometheus
 // observation data stored locally
 func CreateLocalPromsumTable(queryer Queryer, tableName string) error {
 	query := createTable(tableName, "", "", "", nil, promsumColumns, nil, false, true)
