9eb500ccef46e0030e060edd0f088fd1d1ccaa6c
Chance Zibolski <chance.zibolski@coreos.com>
2017-12-15 14:07:25 -0800

    hack/update-codegen.sh: Use k8s.io/code-generator/generate-groups.sh

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -20,68 +20,18 @@ set -o pipefail
 
 SCRIPT_PACKAGE=github.com/coreos-inc/kube-chargeback
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")/..
-SCRIPT_BASE=${SCRIPT_ROOT}
-# SCRIPT_BASE=${SCRIPT_ROOT}/../..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo k8s.io/code-generator)}
 
-clientgen="${PWD}/client-gen-binary"
-listergen="${PWD}/lister-gen"
-informergen="${PWD}/informer-gen"
-deepcopygen="${PWD}/deepcopy-gen"
-# Register function to be called on EXIT to remove generated binary.
-function cleanup {
-  rm -f "${clientgen:-}"
-  rm -f "${listergen:-}"
-  rm -f "${informergen:-}"
-  rm -f "${deepcopygen:-}"
-}
-trap cleanup EXIT
+set -x
 
-function generate_group() {
-  local GROUP_NAME=$1
-  local VERSION=$2
+${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+    "${SCRIPT_PACKAGE}/pkg/generated" "${SCRIPT_PACKAGE}/pkg/apis" \
+    chargeback:v1alpha1 \
+    --go-header-***REMOVED***le ${SCRIPT_ROOT}/hack/boilerplate.go.txt
 
-  local APIS_PKG=${SCRIPT_PACKAGE}/pkg/apis
-  local CLIENT_PKG=${SCRIPT_PACKAGE}/pkg/generated
-  local CLIENTSET_PKG=${CLIENT_PKG}/clientset
-  local LISTERS_PKG=${CLIENT_PKG}/listers
-  local INFORMERS_PKG=${CLIENT_PKG}/informers
-
-  local INPUT_APIS=(
-    ${GROUP_NAME}/
-    ${GROUP_NAME}/${VERSION}
-  )
-
-
-  echo "Building deepcopy-gen"
-  go build -o "${deepcopygen}" ${CODEGEN_PKG}/cmd/deepcopy-gen
-
-  echo "generating deepcopy funcs for group ${GROUP_NAME} and version ${VERSION}"
-  ${deepcopygen} --input-dirs ${APIS_PKG}/${GROUP_NAME}/${VERSION} -O zz_generated.deepcopy --go-header-***REMOVED***le $SCRIPT_ROOT/vendor/k8s.io/gengo/boilerplate/no-boilerplate.go.txt
-
-  echo "Building client-gen"
-  go build -o "${clientgen}" ${CODEGEN_PKG}/cmd/client-gen
-
-  echo "generating clientset for group ${GROUP_NAME} and version ${VERSION} at ${CLIENTSET_PKG}"
-  ${clientgen} --clientset-name="versioned" --input-base ${APIS_PKG} --input ${GROUP_NAME}/${VERSION} --clientset-path ${CLIENTSET_PKG} --go-header-***REMOVED***le $SCRIPT_ROOT/vendor/k8s.io/gengo/boilerplate/no-boilerplate.go.txt
-
-
-  echo "Building lister-gen"
-  go build -o "${listergen}" ${CODEGEN_PKG}/cmd/lister-gen
-
-  echo "generating listers for group ${GROUP_NAME} and version ${VERSION} at ${LISTERS_PKG}"
-  ${listergen} --input-dirs ${APIS_PKG}/${GROUP_NAME} --input-dirs ${APIS_PKG}/${GROUP_NAME}/${VERSION} --output-package ${LISTERS_PKG} --go-header-***REMOVED***le $SCRIPT_ROOT/vendor/k8s.io/gengo/boilerplate/no-boilerplate.go.txt
-
-  echo "Building informer-gen"
-  go build -o "${informergen}" ${CODEGEN_PKG}/cmd/informer-gen
-
-  echo "generating informers for group ${GROUP_NAME} and version ${VERSION} at ${INFORMERS_PKG}"
-  ${informergen} \
-    --input-dirs ${APIS_PKG}/${GROUP_NAME} --input-dirs ${APIS_PKG}/${GROUP_NAME}/${VERSION} \
-    --versioned-clientset-package ${CLIENTSET_PKG}/versioned \
-    --listers-package ${LISTERS_PKG} \
-    --output-package ${INFORMERS_PKG} \
-    --go-header-***REMOVED***le $SCRIPT_ROOT/vendor/k8s.io/gengo/boilerplate/no-boilerplate.go.txt
-}
-
-generate_group chargeback v1alpha1
+# generate-groups doesn't do defaulters
+echo "Generating defaulters"
+${GOPATH}/bin/defaulter-gen \
+    --input-dirs "${SCRIPT_PACKAGE}/pkg/apis/chargeback/v1alpha1" \
+    -O zz_generated.defaults \
+    --go-header-***REMOVED***le ${SCRIPT_ROOT}/hack/boilerplate.go.txt
