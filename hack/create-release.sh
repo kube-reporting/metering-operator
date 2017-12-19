cb5bdbe832efada1d6201a3e172ba1091bc2a7db
Chance Zibolski <chance.zibolski@coreos.com>
2017-12-18 16:35:56 -0800

    hack/create-release.sh: Make zip only use relative paths

--- hack/create-release.sh
+++ hack/create-release.sh
@@ -1,47 +1,54 @@
 #!/bin/bash -e
 
 OUTFILE=$1
-OUTDIR_NAME="${OUTFILE%%.zip}"
-TEMPDIR="$(mktemp -d)/$OUTDIR_NAME"
+OUTPUT_DIR_NAME="${OUTFILE%%.zip}"
+TMPDIR="$(mktemp -d)"
+OUTPUT_DIR="$TMPDIR/$OUTPUT_DIR_NAME"
 
-mkdir -p "$TEMPDIR"
+mkdir -p "$OUTPUT_DIR"
 
-mkdir -p "$TEMPDIR/hack"
+mkdir -p "$OUTPUT_DIR/hack"
 cp \
     hack/install.sh \
     hack/alm-install.sh \
     hack/install.sh \
     hack/uninstall.sh \
     hack/util.sh \
-    "$TEMPDIR/hack/"
+    "$OUTPUT_DIR/hack/"
 
-mkdir -p "$TEMPDIR/Documentation"
+mkdir -p "$OUTPUT_DIR/Documentation"
 cp \
     Documentation/Installation.md \
     Documentation/Report.md \
     Documentation/Using-chargeback.md \
-    "$TEMPDIR/Documentation/"
+    "$OUTPUT_DIR/Documentation/"
 
-mkdir -p "$TEMPDIR/manifests"
+mkdir -p "$OUTPUT_DIR/manifests"
 cp -r \
     manifests/custom-resources \
-    "$TEMPDIR/manifests/"
+    "$OUTPUT_DIR/manifests/"
 
 # Remove scheduled reports folder since we currently do not support them
-rm -r "$TEMPDIR/manifests/custom-resources/scheduled-reports"
+rm -r "$OUTPUT_DIR/manifests/custom-resources/scheduled-reports"
 
 cp -r \
     manifests/installer \
-    "$TEMPDIR/manifests/"
+    "$OUTPUT_DIR/manifests/"
 
 cp -r \
     manifests/alm \
-    "$TEMPDIR/manifests/"
+    "$OUTPUT_DIR/manifests/"
 
-mkdir -p $TEMPDIR/manifests/chargeback-config
+mkdir -p $OUTPUT_DIR/manifests/chargeback-config
 cp \
     manifests/chargeback-config/custom-values.yaml \
-    "$TEMPDIR/manifests/chargeback-config"
-echo "Start with Documentation/Installation.md" > "$TEMPDIR/README"
+    "$OUTPUT_DIR/manifests/chargeback-config"
+echo "Start with Documentation/Installation.md" > "$OUTPUT_DIR/README"
 
-zip -r "$OUTFILE" "$TEMPDIR"
+
+pushd "$TMPDIR"
+zip -r "$OUTFILE" "$OUTPUT_DIR_NAME"
+popd
+
+mv "$OUTPUT_DIR.zip" .
+
