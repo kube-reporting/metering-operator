399ca42980cc088713d1c254a0b5e6b87ec7505e
Chance Zibolski <chance.zibolski@coreos.com>
2017-12-19 13:17:13 -0800

    hack: Update alm-uninstall.sh to delete deployment by name

--- hack/alm-uninstall.sh
+++ hack/alm-uninstall.sh
@@ -13,7 +13,7 @@ kube-remove \
     manifests/alm/chargeback-clusterserviceversion.yaml
 
 msg "Removing chargeback-helm-operator"
-kube-remove-non-***REMOVED***le deployment -l alm-owner-name=chargeback-helm-operator.v0.5.0
+kube-remove-non-***REMOVED***le deployment chargeback-helm-operator
 
 if [ "$SKIP_DELETE_CRDS" == "true" ]; then
     echo "\$SKIP_DELETE_CRDS is true, skipping deletion of Custom Resource De***REMOVED***nitions"
