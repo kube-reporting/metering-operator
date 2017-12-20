2eb1c6fb34ad1d13e97891eb37d2dc662129d639
Chance Zibolski <chance.zibolski@coreos.com>
2017-12-20 15:35:26 -0800

    Jenkinsfile: Support running integration tests on non-master branches

--- hack/integration-tests.sh
+++ hack/integration-tests.sh
@@ -7,9 +7,13 @@ source ${DIR}/util.sh
 
 export CHARGEBACK_NAMESPACE=${CHARGEBACK_NAMESPACE:-chargeback-ci}
 
+# lowercase the value, since namespaces must be lowercase values
+CHARGEBACK_NAMESPACE=$(echo -n "$CHARGEBACK_NAMESPACE" | tr '[:upper:]' '[:lower:]')
+
 set -x
 go test \
     -v "./test/integration" \
+    -timeout 20m \
     -namespace "${CHARGEBACK_NAMESPACE}" \
     -kubecon***REMOVED***g "${KUBECONFIG}"
 
