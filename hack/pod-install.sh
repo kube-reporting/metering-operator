3e5ebfcdf11f7f98389293c5d7d72cffffaa7fe6
Chance Zibolski <chance.zibolski@coreos.com>
2018-01-17 12:59:45 -0500

    hack: Remove pod-install script
    Rarely used and we're moving to ALM for installation as the only method
    soon.

--- hack/pod-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source ${DIR}/util.sh
-
-msg "Installing chargeback installer"
-kube-install manifests/installer
