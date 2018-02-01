0c00f95cf48daa598682d329644e47d69b5aa00b
Chance Zibolski <chance.zibolski@coreos.com>
2018-02-01 11:45:36 -0800

    hack: Sanitize namespace in delete-ns.sh

--- hack/delete-ns.sh
+++ hack/delete-ns.sh
@@ -5,5 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/util.sh
 
 export CHARGEBACK_NAMESPACE=${CHARGEBACK_NAMESPACE:-chargeback-ci}
+CHARGEBACK_NAMESPACE="$(sanetize_namespace "$CHARGEBACK_NAMESPACE")"
 
 kubectl delete ns --now "$CHARGEBACK_NAMESPACE"
