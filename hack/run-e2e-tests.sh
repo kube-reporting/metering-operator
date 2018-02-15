11b7254f762a4c8cdfa0fd2a030009eee11f2040
Chance Zibolski <chance.zibolski@coreos.com>
2018-02-15 15:39:23 -0800

    hack,test,Jenkinsfile: Move integration tests to e2e and update namespace name

--- hack/run-e2e-tests.sh
+++ hack/run-e2e-tests.sh
@@ -14,7 +14,7 @@ go test \
     -test.short="${CHARGEBACK_SHORT_TESTS}" \
     -test.v \
     -timeout 20m \
-    "./test/integration" \
+    "./test/e2e" \
     -namespace "${CHARGEBACK_NAMESPACE}" \
     -kubeconfig "${KUBECONFIG}" "$@"
 
