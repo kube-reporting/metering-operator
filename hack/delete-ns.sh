558b42794247e149dde821548cbbb26e6c14a08c
Chance Zibolski <chance.zibolski@coreos.com>
2018-02-16 10:59:06 -0800

    hack/delete-ns.sh: ignore namespace not found

--- hack/delete-ns.sh
+++ hack/delete-ns.sh
@@ -7,4 +7,4 @@ source ${DIR}/util.sh
 export CHARGEBACK_NAMESPACE=${CHARGEBACK_NAMESPACE:-chargeback-ci}
 CHARGEBACK_NAMESPACE="$(sanetize_namespace "$CHARGEBACK_NAMESPACE")"
 
-kubectl delete ns --now "$CHARGEBACK_NAMESPACE"
+kubectl delete ns --now --ignore-not-found=true "$CHARGEBACK_NAMESPACE"
