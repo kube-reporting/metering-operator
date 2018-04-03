999cb580a8c395fb2f735e55d0186e05cba4883a
Chance Zibolski <chance.zibolski@coreos.com>
2018-04-03 12:18:36 -0700

    hack: Fix typo in e2e-test-runner

--- hack/e2e-test-runner.sh
+++ hack/e2e-test-runner.sh
@@ -4,7 +4,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/util.sh
 
-: "${KUBECONFIG?}"
+: "${KUBECONFIG:?}"
 : "${CHARGEBACK_NAMESPACE:?}"
 
 : "${TEST_SCRIPT:?}"
