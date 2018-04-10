aaa6e6b46890ed6e5976c201ac6dc3379e0657b9
Chance Zibolski <chance.zibolski@coreos.com>
2018-04-10 15:35:53 -0700

    Gopkg.lock,vendor: Upgrade presto client library

--- vendor/github.com/prestodb/presto-go-client/presto/presto.go
+++ vendor/github.com/prestodb/presto-go-client/presto/presto.go
@@ -61,6 +61,7 @@ import (
 	"math"
 	"net/http"
 	"net/url"
+	"regexp"
 	"strconv"
 	"strings"
 	"sync"
@@ -471,7 +472,7 @@ func (st *driverStmt) QueryContext(ctx context.Context, args []driver.NamedValue
 		conn:    st.conn,
 		nextURI: sr.NextURI,
 	}
-	if err = rows.fetch(); err != nil {
+	if err = rows.fetch(false); err != nil {
 		return nil, err
 	}
 	return rows, nil
@@ -485,7 +486,7 @@ type driverRows struct {
 	err      error
 	rowindex int
 	columns  []string
-	coltype  []driver.ValueConverter
+	coltype  []*typeConverter
 	data     []queryData
 }
 
@@ -521,12 +522,22 @@ func (qr *driverRows) Columns() []string {
 		return []string{}
 	}
 	if qr.columns == nil {
-		if err := qr.fetch(); err != nil {
+		if err := qr.fetch(false); err != nil {
 			qr.err = err
 			return []string{}
 		}
 	}
 	return qr.columns
+}
+
+var coltypeLengthSuf***REMOVED***x = regexp.MustCompile(`\(\d+\)$`)
+
+func (qr *driverRows) ColumnTypeDatabaseTypeName(index int) string {
+	name := qr.coltype[index].typeName
+	if m := coltypeLengthSuf***REMOVED***x.FindStringSubmatch(name); m != nil {
+		name = name[0 : len(name)-len(m[0])]
+	}
+	return name
 }
 
 func (qr *driverRows) Next(dest []driver.Value) error {
@@ -538,7 +549,7 @@ func (qr *driverRows) Next(dest []driver.Value) error {
 			qr.err = io.EOF
 			return qr.err
 		}
-		if err := qr.fetch(); err != nil {
+		if err := qr.fetch(true); err != nil {
 			qr.err = err
 			return err
 		}
@@ -603,7 +614,7 @@ func handleResponseError(status int, respErr stmtError) error {
 	}
 }
 
-func (qr *driverRows) fetch() error {
+func (qr *driverRows) fetch(allowEOF bool) error {
 	req, err := qr.conn.newRequest("GET", qr.nextURI, nil)
 	if err != nil {
 		return err
@@ -627,9 +638,11 @@ func (qr *driverRows) fetch() error {
 	qr.nextURI = qresp.NextURI
 	if len(qr.data) == 0 {
 		if qr.nextURI != "" {
-			return qr.fetch()
+			return qr.fetch(allowEOF)
 		}
-		return io.EOF
+		if allowEOF {
+			return io.EOF
+		}
 	}
 	if qr.columns == nil && len(qresp.Columns) > 0 {
 		qr.initColumns(&qresp)
@@ -639,7 +652,7 @@ func (qr *driverRows) fetch() error {
 
 func (qr *driverRows) initColumns(qresp *queryResponse) {
 	qr.columns = make([]string, len(qresp.Columns))
-	qr.coltype = make([]driver.ValueConverter, len(qresp.Columns))
+	qr.coltype = make([]*typeConverter, len(qresp.Columns))
 	for i, col := range qresp.Columns {
 		qr.columns[i] = col.Name
 		qr.coltype[i] = newTypeConverter(col.Type)
@@ -684,7 +697,7 @@ func (c *typeConverter) ConvertValue(v interface{}) (driver.Value, error) {
 			return nil, err
 		}
 		return vv.Bool, err
-	case "json", "char", "varchar", "varbinary", "interval year to month", "interval day to second", "decimal":
+	case "json", "char", "varchar", "varbinary", "interval year to month", "interval day to second", "decimal", "unknown":
 		vv, err := scanNullString(v)
 		if !vv.Valid {
 			return nil, err
@@ -1036,11 +1049,19 @@ func scanNullFloat64(v interface{}) (sql.NullFloat64, error) {
 		return sql.NullFloat64{}, nil
 	}
 	vv, ok := v.(float64)
-	if !ok {
-		return sql.NullFloat64{},
-			fmt.Errorf("cannot convert %v (%T) to float64", v, v)
+	if ok {
+		return sql.NullFloat64{Valid: true, Float64: vv}, nil
 	}
-	return sql.NullFloat64{Valid: true, Float64: vv}, nil
+	switch v {
+	case "NaN":
+		return sql.NullFloat64{Valid: true, Float64: math.NaN()}, nil
+	case "In***REMOVED***nity":
+		return sql.NullFloat64{Valid: true, Float64: math.Inf(+1)}, nil
+	case "-In***REMOVED***nity":
+		return sql.NullFloat64{Valid: true, Float64: math.Inf(-1)}, nil
+	default:
+		return sql.NullFloat64{}, fmt.Errorf("cannot convert %v (%T) to float64", v, v)
+	}
 }
 
 // NullSliceFloat64 represents a slice of float64 that may be null.
