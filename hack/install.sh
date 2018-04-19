5a5cb9ca4af8e2f66290e3b5d16d6165d23eb074
Chance Zibolski <chance.zibolski@coreos.com>
2018-04-19 15:23:57 -0700

    hack/install.sh: Fix skip copy pull-secret and path to CRDs

--- hack/install.sh
+++ hack/install.sh
@@ -4,9 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${DIR}/default-env.sh"
 source "${DIR}/util.sh"
 
+MANIFESTS_DIR="$DIR/../manifests"
 : "${CREATE_NAMESPACE:=true}"
 : "${SKIP_COPY_PULL_SECRET:=false}"
-: "${INSTALLER_MANIFEST_DIR:=$DIR/../manifests/installer}"
+: "${INSTALLER_MANIFEST_DIR:=$MANIFESTS_DIR/installer}"
 : "${CHARGEBACK_CR_FILE:=$INSTALLER_MANIFEST_DIR/chargeback.yaml}"
 
 if [ "$CREATE_NAMESPACE" == "true" ]; then
@@ -17,7 +18,7 @@ elif ! kubectl get namespace ${CHARGEBACK_NAMESPACE} 2> /dev/null; then
     exit 1
 fi
 
-if [[ "$SKIP_COPY_PULL_SECRET" != "true" && "$CHARGEBACK_NAMESPACE" != "tectonic-system" ]]; then
+if [[ "$SKIP_COPY_PULL_SECRET" != "true" || "$CHARGEBACK_NAMESPACE" != "tectonic-system" ]]; then
     msg "Configuring pull secrets"
     copy-tectonic-pull
 elif [ -s "$CHARGEBACK_PULL_SECRET_PATH" ]; then
@@ -32,7 +33,7 @@ fi
 
 msg "Installing Custom Resource Definitions"
 kube-install \
-    manifests/custom-resource-definitions
+    "$MANIFESTS_DIR/custom-resource-definitions"
 
 msg "Installing chargeback-helm-operator service account and RBAC resources"
 kube-install \
