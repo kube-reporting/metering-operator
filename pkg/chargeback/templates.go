918ce35328d0d41ff3a123c550ba975c475548cb
Chance Zibolski <chance.zibolski@coreos.com>
2018-04-20 12:42:39 -0700

    pkg/chargeback: Allow ReportGenerationQueries to depend on non-view ReportGenerationQueries
    Also fix query validation and add cycle detection. The code Ranged over
    the wrong variable making it seem as though all queries had no
    dependencies to validate, but it did ensure they existed. Also add depth
    tracking to check for basic cycles on ReportQueries dependencies
    
    pkg/apis: Add DynamicGenerationQueries field to ReportGenerationQuery
    
    For holding dependent generation queries which don't have a view
    available and must be rendered.
    
    pkg/chargeback: Validate and handle dynamic generationQueries in getDependentGenerationQueries

--- pkg/chargeback/templates.go
+++ pkg/chargeback/templates.go
@@ -15,17 +15,9 @@ const (
 	PrestoTimestampFormat = "2006-01-02 15:04:05.000"
 )
 
-var templateFuncMap = template.FuncMap{
-	"hiveAWSPartitionTimestamp": hiveAWSPartitionTimestamp,
-	"prestoTimestamp":           prestoTimestamp,
-	"dataSourceTableName":       dataSourceTableName,
-	"generationQueryViewName":   generationQueryViewName,
-	"billingPeriodFormat":       billingPeriodFormat,
-	"filterAWSData":             filterAWSData,
-}
-
 type templateInfo struct {
-	Report *reportTemplateInfo
+	Report                  *reportTemplateInfo
+	DynamicDependentQueries []*cbTypes.ReportGenerationQuery
 }
 
 type reportTemplateInfo struct {
@@ -34,6 +26,16 @@ type reportTemplateInfo struct {
 }
 
 func newQueryTemplate(queryTemplate string) (*template.Template, error) {
+	var templateFuncMap = template.FuncMap{
+		"hiveAWSPartitionTimestamp":   hiveAWSPartitionTimestamp,
+		"prestoTimestamp":             prestoTimestamp,
+		"dataSourceTableName":         dataSourceTableName,
+		"generationQueryViewName":     generationQueryViewName,
+		"billingPeriodFormat":         billingPeriodFormat,
+		"filterAWSData":               filterAWSData,
+		"renderReportGenerationQuery": renderReportGenerationQuery,
+	}
+
 	tmpl, err := template.New("report-generation-query").Delims("{|", "|}").Funcs(templateFuncMap).Parse(queryTemplate)
 	if err != nil {
 		return nil, fmt.Errorf("error parsing query: %v", err)
@@ -41,35 +43,44 @@ func newQueryTemplate(queryTemplate string) (*template.Template, error) {
 	return tmpl, nil
 }
 
-func renderReportGenerationQuery(startPeriod, endPeriod time.Time, queryStr string) (string, error) {
-	tmpl, err := newQueryTemplate(queryStr)
+type queryRenderer struct {
+	templateInfo *templateInfo
+}
+
+func (qr queryRenderer) Render(query string) (string, error) {
+	tmpl, err := newQueryTemplate(query)
 	if err != nil {
 		return "", err
 	}
-	info := &templateInfo{
-		Report: &reportTemplateInfo{
-			StartPeriod: startPeriod,
-			EndPeriod:   endPeriod,
-		},
-	}
-	return renderTemplateInfo(tmpl, info)
+	return qr.renderTemplate(tmpl)
 }
 
-func renderGenerationQuery(generationQuery *cbTypes.ReportGenerationQuery) (string, error) {
-	tmpl, err := newQueryTemplate(generationQuery.Spec.Query)
-	if err != nil {
-		return "", err
-	}
-	return renderTemplateInfo(tmpl, nil)
-}
-
-func renderTemplateInfo(tmpl *template.Template, info *templateInfo) (string, error) {
+func (qr queryRenderer) renderTemplate(tmpl *template.Template) (string, error) {
 	var buf bytes.Buffer
-	err := tmpl.Execute(&buf, info)
+	err := tmpl.Execute(&buf, qr.templateInfo)
 	if err != nil {
 		return "", fmt.Errorf("error executing template: %v", err)
 	}
 	return buf.String(), nil
+}
+
+func renderReportGenerationQuery(generationQueryName string, templateInfo *templateInfo) (string, error) {
+	var query string
+	for _, q := range templateInfo.DynamicDependentQueries {
+		if q.Name == generationQueryName {
+			query = q.Spec.Query
+			break
+		}
+	}
+	if query == "" {
+		return "", fmt.Errorf("unknown generationQuery %s", generationQueryName)
+	}
+	qr := queryRenderer{templateInfo: templateInfo}
+	renderedQuery, err := qr.Render(query)
+	if err != nil {
+		return "", fmt.Errorf("unable to render query %s, err: %v", generationQueryName, err)
+	}
+	return renderedQuery, nil
 }
 
 func hiveAWSPartitionTimestamp(date time.Time) string {
@@ -94,11 +105,11 @@ func filterAWSData(r *reportTemplateInfo, awsBillingDataSourceName string) strin
 
                    -- AWS data covers start to middle
                    WHEN (aws_billing.usage_start_date <= timestamp '%s')
-                       THEN cast(date_diff('millisecond', timestamp '%s', aws_billing.usage_end_date) as double) / cast(date_diff('millisecond', aws_billing.usage_start_date, aws_billing.usage_end_date) as double)
+                       THEN cast(date_diff('millisecond', timestamp '%s', aws_billing.usage_end_date) as double) / cast(date_diff('millisecond', aws_billing.usage_start_date, aws_billing.usage_end_date) as double
 
                    -- AWS data covers middle to end
                    WHEN ( timestamp '%s' <= aws_billing.usage_end_date)
-                       THEN cast(date_diff('millisecond', aws_billing.usage_start_date, timestamp '%s') as double) / cast(date_diff('millisecond', aws_billing.usage_start_date, aws_billing.usage_end_date) as double)
+                       THEN cast(date_diff('millisecond', aws_billing.usage_start_date, timestamp '%s') as double) / cast(date_diff('millisecond', aws_billing.usage_start_date, aws_billing.usage_end_date) as doub
                    ELSE 1
                END as period_percent
         FROM %s as aws_billing
