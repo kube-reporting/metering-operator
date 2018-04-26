ea651741157776adfa8f2cf94f3ffd23e9a0ccc6
Chance Zibolski <chance.zibolski@coreos.com>
2018-04-26 10:21:54 -0700

    hack: Update install scripts to use new manifest locations

--- hack/alm-install.sh
+++ hack/alm-install.sh
@@ -4,8 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${DIR}/default-env.sh"
 source "${DIR}/util.sh"
 
-: "${INSTALLER_MANIFEST_DIR:=$DIR/../manifests/installer}"
-: "${CHARGEBACK_CR_FILE:=$INSTALLER_MANIFEST_DIR/chargeback.yaml}"
+MANIFESTS_DIR="$DIR/../manifests"
+: "${INSTALLER_MANIFEST_DIR:=$MANIFESTS_DIR/deploy/tectonic/helm-operator}"
+: "${ALM_MANIFEST_DIR:=$MANIFESTS_DIR/deploy/tectonic/alm}"
+: "${CHARGEBACK_CR_FILE:=$INSTALLER_MANIFEST_DIR/metering.yaml}"
 
 kubectl create namespace "${CHARGEBACK_NAMESPACE}" || true
 
@@ -16,11 +18,11 @@ fi
 
 msg "Installing Custom Resource Definitions"
 kube-install \
-    manifests/custom-resource-definitions
+    "$MANIFESTS_DIR/custom-resource-definitions"
 
-msg "Installing Chargeback Cluster Service Version"
+msg "Installing Metering Cluster Service Version"
 kube-install \
-    manifests/alm/chargeback.clusterserviceversion.yaml
+    "$ALM_MANIFEST_DIR/metering.clusterserviceversion.yaml"
 
 msg "Installing Chargeback Resource"
 kube-install \
--- hack/openshift-install.sh
+++ hack/openshift-install.sh
@@ -8,4 +8,7 @@ if command -v oc; then
     oc new-project "${CHARGEBACK_NAMESPACE}" || oc project "${CHARGEBACK_NAMESPACE}"
 fi
 
+export INSTALLER_MANIFEST_DIR="${INSTALLER_MANIFEST_DIR:-$MANIFESTS_DIR/deploy/openshift/helm-operator}"
+export CHARGEBACK_CR_FILE="${CHARGEBACK_CR_FILE:-$INSTALLER_MANIFEST_DIR/metering.yaml}"
+
 "${DIR}/install.sh"
