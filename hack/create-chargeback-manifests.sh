1907987067b7b746fe0cf83ca8d69d6cc22aaeaa
Chance Zibolski <chance.zibolski@coreos.com>
2018-04-26 10:52:40 -0700

    hack,manifests: Add openshift helm and ALM manifests

--- hack/create-chargeback-manifests.sh
+++ hack/create-chargeback-manifests.sh
@@ -12,12 +12,16 @@ trap "rm -rf $TMPDIR" EXIT
 "$DIR/render-helm-operator-override-values.sh" "$1" > "$TMPDIR/override-helm-operator-values.yaml"
 "$DIR/render-alm-override-values.sh" "$1" > "$TMPDIR/override-alm-values.yaml"
 
+# tectonic
+echo "Creating Tectonic deploy manifests"
 "$DIR/create-deploy-manifests.sh" \
     "$DEPLOY_DIR/tectonic/helm-operator" \
     "$DEPLOY_DIR/common-helm-operator-values.yaml" \
     "$DEPLOY_DIR/tectonic/helm-operator-values.yaml" \
     "$TMPDIR/override-helm-operator-values.yaml"
 
+echo
+echo "Creating Tectonic ALM manifests"
 "$DIR/create-alm-csv-manifests.sh" \
     "$DEPLOY_DIR/tectonic/helm-operator" \
     "$DEPLOY_DIR/tectonic/alm" \
@@ -25,3 +29,20 @@ trap "rm -rf $TMPDIR" EXIT
     "$DEPLOY_DIR/tectonic/alm-values.yaml" \
     "$TMPDIR/override-alm-values.yaml"
 
+# openshift
+echo
+echo "Creating Openshift deploy manifests"
+"$DIR/create-deploy-manifests.sh" \
+    "$DEPLOY_DIR/openshift/helm-operator" \
+    "$DEPLOY_DIR/common-helm-operator-values.yaml" \
+    "$DEPLOY_DIR/openshift/helm-operator-values.yaml" \
+    "$TMPDIR/override-helm-operator-values.yaml"
+
+echo
+echo "Creating Openshift ALM manifests"
+"$DIR/create-alm-csv-manifests.sh" \
+    "$DEPLOY_DIR/openshift/helm-operator" \
+    "$DEPLOY_DIR/openshift/alm" \
+    "$DEPLOY_DIR/common-alm-values.yaml" \
+    "$DEPLOY_DIR/openshift/alm-values.yaml" \
+    "$TMPDIR/override-alm-values.yaml"
