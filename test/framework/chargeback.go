7f0540c62a6060eb68b376011e10570dcef8bdc6
Chance Zibolski <chance.zibolski@coreos.com>
2018-04-30 13:43:39 -0700

    test: Update metering service name in tests to "metering" from "chargeback"

--- test/framework/chargeback.go
+++ test/framework/chargeback.go
@@ -5,18 +5,20 @@ import (
 	restclient "k8s.io/client-go/rest"
 )
 
-func (f *Framework) NewChargebackSVCRequest(svcName, endpoint string, query map[string]string) *restclient.Request {
-	wrapper := f.KubeClient.CoreV1().Services(f.Namespace).ProxyGet("", svcName, "8080", endpoint, query)
+const chargebackServiceName = "metering"
+
+func (f *Framework) NewChargebackSVCRequest(endpoint string, query map[string]string) *restclient.Request {
+	wrapper := f.KubeClient.CoreV1().Services(f.Namespace).ProxyGet("", chargebackServiceName, "8080", endpoint, query)
 	return wrapper.(*restclient.Request)
 }
 
-func (f *Framework) NewChargebackSVCPOSTRequest(svcName, endpoint string, body interface{}) *restclient.Request {
+func (f *Framework) NewChargebackSVCPOSTRequest(endpoint string, body interface{}) *restclient.Request {
 	return f.KubeClient.CoreV1().RESTClient().
 		Post().
 		Namespace(f.Namespace).
 		Resource("services").
 		SubResource("proxy").
-		Name(net.JoinSchemeNamePort("", svcName, "8080")).
+		Name(net.JoinSchemeNamePort("", chargebackServiceName, "8080")).
 		Suffix(endpoint).
 		Body(body)
 }
