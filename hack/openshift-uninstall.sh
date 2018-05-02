6479148993078f100bad92381fc6fe855bac5865
Chance Zibolski <chance.zibolski@coreos.com>
2018-05-02 16:35:28 -0700

    hack: Add scripts for installing/uninstalling based on platform

--- hack/openshift-uninstall.sh
+++ hack/openshift-uninstall.sh
@@ -4,9 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${DIR}/default-env.sh"
 source "${DIR}/util.sh"
 
-if command -v oc; then
-    oc new-project "${METERING_NAMESPACE}" || oc project "${METERING_NAMESPACE}"
-fi
-
 export DEPLOY_PLATFORM=openshift
-"${DIR}/install.sh"
+"${DIR}/uninstall.sh"
