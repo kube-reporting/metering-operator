dfc41e5ec0d241d685e321cb54b3f88592b8e7b3
Chance Zibolski <chance.zibolski@coreos.com>
2018-05-09 13:08:37 -0700

    hack,Makefile: Add helper script for building & running operator locally

---
+++ build/check_defined.mk
@@ -0,0 +1,14 @@
+# https://stackoverflow.com/questions/10858261/abort-makefile-if-variable-not-set
+# Check that given variables are set and all have non-empty values,
+# die with an error otherwise.
+#
+# Params:
+#   1. Variable name(s) to test.
+#   2. (optional) Error message to print.
+check_defined = \
+    $(strip $(foreach 1,$1, \
+        $(call __check_defined,$1,$(strip $(value 2)))))
+__check_defined = \
+    $(if $(value $1),, \
+        $(error Undefined $1$(if $2, ($2))$(if $(value @), \
+                required by target `$@')))
