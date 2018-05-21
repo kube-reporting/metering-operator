3339918b8c863a735343d52299de8a4de95bea87
Chance Zibolski <chance.zibolski@coreos.com>
2018-05-21 16:38:22 -0700

    hack: Install ClusterRole/ClusterRoleBinding granting access to Prometheus in Openshift
    Cluster monitoring has an RBAC based auth proxy which requires our
    service-account to have access to GET namespaces. To handle this in the
    short-term this is handled in hack/openshift-install.sh and
    hack/openshift-uninstall.sh.
    
    For context, the follow link is where the source of the RBAC permissions
    required is located:
    https://github.com/openshift/cluster-monitoring-operator/blob/52105c47f7dc4497664a77668269a3d499f85b6b/assets/prometheus-k8s/prometheus-k8s.yaml#L60

--- hack/openshift-install.sh
+++ hack/openshift-install.sh
@@ -3,8 +3,26 @@
 ROOT_DIR=$(dirname "${BASH_SOURCE}")/..
 source "${ROOT_DIR}/hack/common.sh"
 
+: "${METERING_INSTALL_NAMESPACE_VIEWER_CLUSTERROLE:=true}"
+
 if command -v oc; then
     oc new-project "${METERING_NAMESPACE}" || oc project "${METERING_NAMESPACE}"
+fi
+
+if [ "$METERING_INSTALL_NAMESPACE_VIEWER_CLUSTERROLE" == "true" ]; then
+    kubectl -n "${METERING_NAMESPACE}" \
+        create clusterrole \
+        metering-namespace-viewer \
+        --verb=get \
+        --resource=namespaces || true
+
+    kubectl -n "${METERING_NAMESPACE}" \
+        create clusterrolebinding \
+        metering-namespace-viewer \
+        --clusterrole \
+        metering-namespace-viewer \
+        --serviceaccount \
+        "${METERING_NAMESPACE}:metering" || true
 fi
 
 export DEPLOY_PLATFORM=openshift
