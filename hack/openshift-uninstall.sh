bc237cb15338cfb426b21821eacb43832b5b9f8e
Chance Zibolski <chance.zibolski@coreos.com>
2018-05-21 16:32:21 -0700

    hack: Fix path to install/uninstall scripts in openshift specific scripts

--- hack/openshift-uninstall.sh
+++ hack/openshift-uninstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 ROOT_DIR=$(dirname "${BASH_SOURCE}")/..
+source "${ROOT_DIR}/hack/common.sh"
 
 export DEPLOY_PLATFORM=openshift
-"${ROOT_DIR}/uninstall.sh"
+"${ROOT_DIR}/hack/uninstall.sh"
