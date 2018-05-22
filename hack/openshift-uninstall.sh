84228ea87e69870c6dfa8113cc8e4eafd71cb6fa
Chance Zibolski <chance.zibolski@coreos.com>
2018-05-22 11:20:07 -0700

    hack: Prefix metering-namespace-viewer ClusterRoleBinding with $METERING_NAMESPACE
    Ensures that it can always be created and point to the metering operator
    service account in each namespace.

--- hack/openshift-uninstall.sh
+++ hack/openshift-uninstall.sh
@@ -3,11 +3,8 @@
 ROOT_DIR=$(dirname "${BASH_SOURCE}")/..
 source "${ROOT_DIR}/hack/common.sh"
 
-: "${METERING_UNINSTALL_NAMESPACE_VIEWER_CLUSTERROLE:=false}"
-
 export DEPLOY_PLATFORM=openshift
 "${ROOT_DIR}/hack/uninstall.sh"
-
 
 if [ "$METERING_UNINSTALL_NAMESPACE_VIEWER_CLUSTERROLE" == "true" ]; then
     kubectl -n "${METERING_NAMESPACE}" \
