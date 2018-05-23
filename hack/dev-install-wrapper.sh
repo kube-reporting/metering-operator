f02b46a738f37b8fcc4056cc21affecffacd238b
Chance Zibolski <chance.zibolski@coreos.com>
2018-05-23 16:32:46 -0700

    hack: Add installer wrapper script for installing custom metering-helm-operator image

---
+++ hack/dev-install-wrapper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ROOT_DIR=$(dirname "${BASH_SOURCE[0]}")/..
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 [metering-helm-operator-image-tag] [install-script] [args]"
+    echo "Must specify at least two args, metering-helm-operator image tag, and the install script to run"
+    exit 1
+fi
+
+HELM_OPERATOR_IMAGE_TAG="$1"
+shift
+
+TMP="$(mktemp -d)"
+
+trap "rm -rf $TMP" EXIT SIGINT
+
+"$ROOT_DIR/hack/create-metering-manifests.sh" "$HELM_OPERATOR_IMAGE_TAG" "$TMP"
+
+"$@"
