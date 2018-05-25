5c14964420700ed8681cc0a3dc52ee0f4623549d
Chance Zibolski <chance.zibolski@coreos.com>
2018-05-25 10:25:05 -0700

    hack/dev-install-wrapper.sh: Support passing CUSTOM_HELM_OPERATOR_OVERRIDE_VALUES and CUSTOM_ALM_OVERRIDE_VALUES

--- hack/dev-install-wrapper.sh
+++ hack/dev-install-wrapper.sh
@@ -5,20 +5,22 @@ set -o pipefail
 
 ROOT_DIR=$(dirname "${BASH_SOURCE[0]}")/..
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 [metering-helm-operator-image-tag] [install-script] [args]"
-    echo "Must specify at least two args, metering-helm-operator image tag, and the install script to run"
-    exit 1
-fi
-
-HELM_OPERATOR_IMAGE_TAG="$1"
-shift
-
 TMP="$(mktemp -d)"
-
 trap "rm -rf $TMP" EXIT SIGINT
 
-"$ROOT_DIR/hack/create-metering-manifests.sh" "$HELM_OPERATOR_IMAGE_TAG" "$TMP"
+if [[ -n "${CUSTOM_HELM_OPERATOR_OVERRIDE_VALUES-}" && -n "${CUSTOM_ALM_OVERRIDE_VALUES-}" ]]; then
+    "$ROOT_DIR/hack/create-metering-manifests.sh" "$TMP"
+else
+    if [ $# -lt 2 ]; then
+        echo "Usage: $0 [metering-helm-operator-image-tag] [install-script] [args]"
+        echo "Must specify at least two args, metering-helm-operator image tag, and the install script to run"
+        exit 1
+    fi
+
+    HELM_OPERATOR_IMAGE_TAG="$1"
+    shift
+    "$ROOT_DIR/hack/create-metering-manifests.sh" "$HELM_OPERATOR_IMAGE_TAG" "$TMP"
+fi
 
 export DEPLOY_MANIFESTS_DIR="$TMP"
 "$@"
