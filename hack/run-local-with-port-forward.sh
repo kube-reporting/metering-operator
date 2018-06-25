ef8bdcd0da74a3c7bd7084d2f9a5b8a1a9f08d2a
Chance Zibolski <chance.zibolski@coreos.com>
2018-06-25 16:41:41 -0700

    hack: Fix killing port-forwards in run-local

--- hack/run-local-with-port-forward.sh
+++ hack/run-local-with-port-forward.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 ROOT_DIR=$(dirname "${BASH_SOURCE}")/..
 source "${ROOT_DIR}/hack/common.sh"
@@ -9,22 +9,21 @@ source "${ROOT_DIR}/hack/common.sh"
 : "${METERING_PROMETHEUS_NAMESPACE:=tectonic-system}"
 : "${METERING_PROMTHEUS_LABEL_SELECTOR:=app=prometheus}"
 
-trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
+set -e -o pipefail
+trap 'jobs -p | xargs kill' EXIT
+
+PRESTO="$(kubectl get pods -n "$METERING_NAMESPACE" -l app=presto,presto=coordinator -o name | cut -d/ -f2)"
+HIVE="$(kubectl get pods -n "$METERING_NAMESPACE" -l app=hive,hive=server -o name | cut -d/ -f2)"
+PROM="$(kubectl get pods -n "$METERING_PROMETHEUS_NAMESPACE" -l "$METERING_PROMTHEUS_LABEL_SELECTOR" -o name | cut -d/ -f2)"
 
 echo Starting presto port-forward
-kubectl get pods -n "$METERING_NAMESPACE" -l app=presto,presto=coordinator -o name \
-    | cut -d/ -f2 \
-    | xargs -I{} kubectl -n "$METERING_NAMESPACE" port-forward {} 9991:8080 &
+kubectl -n "$METERING_NAMESPACE" port-forward "$PRESTO" 9991:8080 &
 
 echo Starting hive port-forward
-kubectl get pods -n "$METERING_NAMESPACE" -l app=hive -o name \
-    | cut -d/ -f2 \
-    | xargs -I{} kubectl -n "$METERING_NAMESPACE" port-forward {} 9992:10000 &
+kubectl -n "$METERING_NAMESPACE" port-forward "$HIVE" 9992:10000 &
 
 echo Starting Prometheus port-forward
-kubectl -n "$METERING_PROMETHEUS_NAMESPACE" get pods -l "$METERING_PROMTHEUS_LABEL_SELECTOR" -o name \
-    | cut -d/ -f2 \
-    | xargs -I{}  kubectl -n "$METERING_PROMETHEUS_NAMESPACE" port-forward {} 9993:9090 &
+kubectl -n "$METERING_PROMETHEUS_NAMESPACE" port-forward "$PROM" 9993:9090 &
 
 sleep 6
 
