376af5a67a45bbe2be2a01fb88e5816341ff9507
Emily Moss <emoss@redhat.com>
2018-07-25 17:31:48 -0700

    pkg/chargeback: create tableHidden unit tests

--- pkg/chargeback/http_test.go
+++ pkg/chargeback/http_test.go
@@ -362,7 +362,7 @@ func TestAPIV2ReportsGet(t *testing.T) {
 		queryerPrepareFunc func(*mockpresto.MockExecQueryer) []presto.Row
 		expectedResults    *GetReportResults
 	}{
-		"report-finished-with-results-full": {
+		"report-finished-with-results": {
 			reportName: testReportName,
 			report:     newTestReport(testReportName, namespace, testQueryName, reportStart, reportEnd, v1alpha1.ReportStatus{Phase: v1alpha1.ReportPhaseFinished}),
 			query: newTestReportGenQuery(testQueryName, namespace, []v1alpha1.ReportGenerationQueryColumn{
@@ -414,7 +414,7 @@ func TestAPIV2ReportsGet(t *testing.T) {
 				},
 			},
 		},
-		"report-finished-no-results-full": {
+		"report-finished-no-results": {
 			reportName: testReportName,
 			report:     newTestReport(testReportName, namespace, testQueryName, reportStart, reportEnd, v1alpha1.ReportStatus{Phase: v1alpha1.ReportPhaseFinished}),
 			query: newTestReportGenQuery(testQueryName, namespace, []v1alpha1.ReportGenerationQueryColumn{
@@ -586,6 +586,287 @@ func TestAPIV2ReportsGet(t *testing.T) {
 
 			// final string URL
 			finalURL := endpointURLFull.String()
+
+			resp, err := server.Client().Get(finalURL)
+			require.NoError(t, err, "expected making http request to not return error")
+
+			// read the body from the response. it shouldn't be empty, even if
+			// the response had a non-2xx code
+			body, err := ioutil.ReadAll(resp.Body)
+			require.NoError(t, err, "expected read all of resp.Body to succeed")
+
+			assert.Equal(t, tt.expectedStatusCode, resp.StatusCode, "Expected http status code to match")
+			t.Logf("response body: %s", string(body))
+
+			// if tt.expectedAPIError is non-empty, then this test is testing the
+			// error response from the API, otherwise it's testing the results
+			// come back with the correct number of items
+			if tt.expectedAPIError != "" {
+				var errResp errorResponse
+				err = json.Unmarshal(body, &errResp)
+				assert.NoError(t, err, "expected unmarshal to not error")
+				assert.Contains(t, errResp.Error, tt.expectedAPIError, "expected error response to contain expected api error")
+			} else {
+				var results GetReportResults
+				err = json.Unmarshal(body, &results)
+				assert.NoError(t, err, "expected unmarshal to not error")
+				// TODO(chance): check more than the results length matching
+				assert.Len(t, results.Results, len(tt.expectedResults.Results), "expected API results length to match expected results length")
+			}
+		})
+	}
+}
+
+//for v2 endpoints TableHidden
+func apiReportV2URLTable(reportName string) string {
+	return path.Join(APIV2Reports, reportName, "table")
+}
+
+func TestAPIV2ReportsTable(t *testing.T) {
+	const namespace = "default"
+	const testReportName = "test-report"
+	const testQueryName = "test-query"
+	reportStart := time.Time{}
+	reportEnd := reportStart.AddDate(0, 1, 0)
+
+	tests := map[string]struct {
+		reportStatus v1alpha1.ReportStatus
+		reportName   string
+
+		report      *v1alpha1.Report
+		query       *v1alpha1.ReportGenerationQuery
+		prestoTable *v1alpha1.PrestoTable
+
+		expectedStatusCode int
+		expectedAPIError   string
+
+		queryerPrepareFunc func(*mockpresto.MockExecQueryer) []presto.Row
+		expectedResults    *GetReportResults
+	}{
+		"report-finished-with-results": {
+			reportName: testReportName,
+			report:     newTestReport(testReportName, namespace, testQueryName, reportStart, reportEnd, v1alpha1.ReportStatus{Phase: v1alpha1.ReportPhaseFinished}),
+			query: newTestReportGenQuery(testQueryName, namespace, []v1alpha1.ReportGenerationQueryColumn{
+				{
+					Name:        "timestamp",
+					Type:        "timestamp",
+					TableHidden: true,
+				},
+				{
+					Name:        "foo",
+					Type:        "double",
+					TableHidden: false,
+				},
+			},
+			),
+			prestoTable: newTestPrestoTable(testReportName, namespace, []hive.Column{
+				{
+					Name: "timestamp",
+					Type: "timestamp",
+				},
+				{
+					Name: "foo",
+					Type: "double",
+				},
+			},
+			),
+			queryerPrepareFunc: func(mock *mockpresto.MockExecQueryer) []presto.Row {
+				result := []presto.Row{
+					{
+						"timestamp": time.Time{},
+						"foo":       1,
+					},
+				}
+				mock.EXPECT().Query(gomock.Any()).Return(result, nil)
+				return result
+			},
+			expectedStatusCode: http.StatusOK,
+			expectedResults: &GetReportResults{
+				Results: []ReportResultEntry{
+					{
+						Values: []ReportResultValues{
+							{
+								Name:        "foo",
+								Value:       1,
+								TableHidden: false,
+							},
+						},
+					},
+				},
+			},
+		},
+		"report-finished-no-results": {
+			reportName: testReportName,
+			report:     newTestReport(testReportName, namespace, testQueryName, reportStart, reportEnd, v1alpha1.ReportStatus{Phase: v1alpha1.ReportPhaseFinished}),
+			query: newTestReportGenQuery(testQueryName, namespace, []v1alpha1.ReportGenerationQueryColumn{
+				{
+					Name:        "timestamp",
+					Type:        "timestamp",
+					TableHidden: true,
+				},
+				{
+					Name:        "foo",
+					Type:        "double",
+					TableHidden: false,
+				},
+			}),
+			prestoTable: newTestPrestoTable(testReportName, namespace, []hive.Column{
+				{
+					Name: "timestamp",
+					Type: "timestamp",
+				},
+				{
+					Name: "foo",
+					Type: "double",
+				},
+			}),
+			queryerPrepareFunc: func(mock *mockpresto.MockExecQueryer) []presto.Row {
+				mock.EXPECT().Query(gomock.Any()).Return(nil, nil)
+				return nil
+			},
+			expectedResults:    &GetReportResults{},
+			expectedStatusCode: http.StatusOK,
+		},
+		"report-finished-db-errored": {
+			reportName: testReportName,
+			report:     newTestReport(testReportName, namespace, testQueryName, reportStart, reportEnd, v1alpha1.ReportStatus{Phase: v1alpha1.ReportPhaseFinished}),
+			query: newTestReportGenQuery(testQueryName, namespace, []v1alpha1.ReportGenerationQueryColumn{
+				{
+					Name:        "timestamp",
+					Type:        "timestamp",
+					TableHidden: true,
+				},
+				{
+					Name:        "foo",
+					Type:        "double",
+					TableHidden: false,
+				},
+			}),
+			prestoTable: newTestPrestoTable(testReportName, namespace, []hive.Column{
+				{
+					Name: "timestamp",
+					Type: "timestamp",
+				},
+				{
+					Name: "foo",
+					Type: "double",
+				},
+			}),
+			queryerPrepareFunc: func(mock *mockpresto.MockExecQueryer) []presto.Row {
+				dbErr := errors.New("mock database had an error")
+				mock.EXPECT().Query(gomock.Any()).Return(nil, dbErr)
+				return nil
+			},
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedAPIError:   "failed to perform presto query",
+		},
+		"non-existent-report": {
+			reportName:         "doesnt-exist",
+			expectedStatusCode: http.StatusNotFound,
+			expectedAPIError:   "not found",
+		},
+		"mismatched-results-schema-to-table-schema": {
+			reportName: testReportName,
+			report:     newTestReport(testReportName, namespace, testQueryName, reportStart, reportEnd, v1alpha1.ReportStatus{Phase: v1alpha1.ReportPhaseFinished}),
+			query: newTestReportGenQuery(testQueryName, namespace, []v1alpha1.ReportGenerationQueryColumn{
+				{
+					Name:        "timestamp",
+					Type:        "timestamp",
+					TableHidden: true,
+				},
+				{
+					Name:        "foo",
+					Type:        "double",
+					TableHidden: false,
+				},
+			}),
+			prestoTable: newTestPrestoTable(testReportName, namespace, []hive.Column{
+				{
+					Name: "timestamp",
+					Type: "timestamp",
+				},
+				{
+					Name: "foo",
+					Type: "double",
+				},
+			}),
+			queryerPrepareFunc: func(mock *mockpresto.MockExecQueryer) []presto.Row {
+				result := []presto.Row{
+					{
+						"timestamp": time.Time{},
+						"foo":       1.5,
+						"this_column_doesnt_exist_in_presto_table": "fail",
+					},
+				}
+				mock.EXPECT().Query(gomock.Any()).Return(result, nil)
+				return result
+			},
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedAPIError:   "results schema doesn't match expected schema",
+		},
+	}
+
+	for testName, tt := range tests {
+		tt := tt
+		testName := testName
+		t.Run(testName, func(t *testing.T) {
+			// manually create indexers which implement the lister interfaces
+			// needed for meteringListers. Since a cache.Indexer is a
+			// cache.Store, it's basically just a key-value store that we can
+			// use to mock the lister returns.
+			reportIndexer := cache.NewIndexer(cache.DeletionHandlingMetaNamespaceKeyFunc, cache.Indexers{})
+			reportGenerationQueryIndexer := cache.NewIndexer(cache.DeletionHandlingMetaNamespaceKeyFunc, cache.Indexers{})
+			prestoTableIndexer := cache.NewIndexer(cache.DeletionHandlingMetaNamespaceKeyFunc, cache.Indexers{})
+
+			listers := meteringListers{
+				reports:                 listers.NewReportLister(reportIndexer).Reports(namespace),
+				reportGenerationQueries: listers.NewReportGenerationQueryLister(reportGenerationQueryIndexer).ReportGenerationQueries(namespace),
+				prestoTables:            listers.NewPrestoTableLister(prestoTableIndexer).PrestoTables(namespace),
+			}
+
+			// add our test report if one is specified
+			if tt.report != nil {
+				reportIndexer.Add(tt.report)
+			}
+			// add our test query for the report
+			if tt.query != nil {
+				reportGenerationQueryIndexer.Add(tt.query)
+			}
+			if tt.prestoTable != nil {
+				prestoTableIndexer.Add(tt.prestoTable)
+			}
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			// setup our mock queryer so we can test without a real database
+			queryer := mockpresto.NewMockExecQueryer(ctrl)
+
+			if tt.queryerPrepareFunc != nil {
+				// we don't need the DB results to verify results in this test
+				_ = tt.queryerPrepareFunc(queryer)
+			}
+
+			// setup a test server suitable for making API calls against
+			router := newRouter(testLogger, queryer, testRand, noopPrometheusImporterFunc, listers)
+			server := httptest.NewServer(router)
+			defer server.Close()
+
+			// set up the query parameters for our API call.
+			// we hardcode format to JSON because validating CSV output is a
+			// bit trickier than JSON
+			params := url.Values{
+				"format": []string{"json"},
+			}
+
+			endpointTable := server.URL + apiReportV2URLTable(tt.reportName)
+			// construct the url object
+			endpointURLTable, err := url.Parse(endpointTable)
+			require.NoError(t, err)
+			endpointURLTable.RawQuery = params.Encode()
+
+			// final string URL
+			finalURL := endpointURLTable.String()
 
 			resp, err := server.Client().Get(finalURL)
 			require.NoError(t, err, "expected making http request to not return error")
