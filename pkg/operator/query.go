a7e24047905047b4ac252a7925fbbf656fcf765c
Chance Zibolski <chance.zibolski@coreos.com>
2018-09-05 16:41:23 -0700

    pkg/operator: Extract out validation of Report dependencies

--- pkg/operator/query.go
+++ pkg/operator/query.go
@@ -2,12 +2,13 @@ package operator
 
 import (
 	"fmt"
-	"strings"
 
-	cbTypes "github.com/operator-framework/operator-metering/pkg/apis/metering/v1alpha1"
 	log "github.com/sirupsen/logrus"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/tools/cache"
+
+	cbTypes "github.com/operator-framework/operator-metering/pkg/apis/metering/v1alpha1"
+	"github.com/operator-framework/operator-metering/pkg/operator/reporting"
 )
 
 func (op *Reporting) runReportGenerationQueryWorker() {
@@ -43,7 +44,8 @@ func (op *Reporting) syncReportGenerationQuery(logger log.FieldLogger, key strin
 
 	logger = logger.WithField("generationQuery", name)
 
-	reportGenerationQuery, err := op.informers.Metering().V1alpha1().ReportGenerationQueries().Lister().ReportGenerationQueries(namespace).Get(name)
+	reportGenerationQueryLister := op.informers.Metering().V1alpha1().ReportGenerationQueries().Lister()
+	reportGenerationQuery, err := reportGenerationQueryLister.ReportGenerationQueries(namespace).Get(name)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
 			logger.Infof("ReportGenerationQuery %s does not exist anymore", key)
@@ -78,19 +80,20 @@ func (op *Reporting) handleReportGenerationQuery(logger log.FieldLogger, generat
 		viewName = generationQuery.ViewName
 	}
 
-	if valid, err := op.validateGenerationQuery(logger, generationQuery, true); err != nil {
-		return err
-	} ***REMOVED*** if !valid {
-		logger.Warnf("cannot create view for reportGenerationQuery, it has uninitialized dependencies")
-		return nil
+	reportDataSourceLister := op.informers.Metering().V1alpha1().ReportDataSources().Lister()
+	reportGenerationQueryLister := op.informers.Metering().V1alpha1().ReportGenerationQueries().Lister()
+
+	depsStatus, err := reporting.GetGenerationQueryDependenciesStatus(reportGenerationQueryLister, reportDataSourceLister, generationQuery)
+	if err != nil {
+		return fmt.Errorf("unable to create view for ReportGenerationQuery %s, failed to retrieve dependencies: %v", generationQuery.Name, err)
+	}
+	validateResults, err := op.validateDependencyStatus(depsStatus)
+	if err != nil {
+		return fmt.Errorf("unable to create view for ReportGenerationQuery %s, failed to validate dependencies %v", generationQuery.Name, err)
 	}
 
-	dependentQueries, err := op.getDependentGenerationQueries(generationQuery, true)
-	if err != nil {
-		return err
-	}
 	templateInfo := &templateInfo{
-		DynamicDependentQueries: dependentQueries,
+		DynamicDependentQueries: validateResults.DynamicReportGenerationQueries,
 		Report:                  nil,
 	}
 
@@ -119,140 +122,26 @@ func (op *Reporting) updateReportQueryViewName(logger log.FieldLogger, generatio
 	return nil
 }
 
-// validateGenerationQuery takes a generationQuery and checks if all of it's
-// dependencies have been initialized. If the generationQuery has a dependency
-// on another generationQuery with spec.view.disabled, the validation will
-// return an error. Returns true if there are no invalid dependencies and all
-// dependencies have a viewName or tableName set in the custom resource.
-// Returns false if there is a dependency that is uninitialized.
-func (op *Reporting) validateGenerationQuery(logger log.FieldLogger, generationQuery *cbTypes.ReportGenerationQuery, queueUninitialized bool) (bool, error) {
-	// Validate ReportGenerationQuery's that should be views
-	generationQueries, err := op.getDependentGenerationQueries(generationQuery, false)
+// validateDependencyStatus runs
+// reporting.ValidateGenerationQueryDependenciesStatus and requeues any
+// uninitialized dependencies
+func (op *Reporting) validateDependencyStatus(dependencyStatus *reporting.GenerationQueryDependenciesStatus) (*reporting.ReportGenerationQueryDependencies, error) {
+	deps, err := reporting.ValidateGenerationQueryDependenciesStatus(dependencyStatus)
 	if err != nil {
-		return false, err
-	}
-
-	// Validate dynamic generationQuery dependencies
-	_, err = op.getDependentGenerationQueries(generationQuery, true)
-	if err != nil {
-		return false, err
-	}
-
-	dataSources, err := op.getDependentDataSources(generationQuery)
-	if err != nil {
-		return false, err
-	}
-	if uninitializedQueries, err := op.getUninitializedReportGenerationQueries(generationQueries); err != nil {
-		return false, err
-	} ***REMOVED*** if len(uninitializedQueries) > 0 {
-		queriesStr := strings.Join(uninitializedQueries, ", ")
-		logger.Warnf("the following ReportGenerationQueries for the query do not have their views created %s", queriesStr)
-		if queueUninitialized {
-			logger.Debugf("queueing uninitializedQueries: %s", queriesStr)
-			for _, query := range uninitializedQueries {
-				key, err := cache.MetaNamespaceKeyFunc(query)
-				if err == nil {
-					op.queues.reportGenerationQueryQueue.Add(key)
-				}
+		for _, query := range dependencyStatus.UninitializedReportGenerationQueries {
+			key, err := cache.MetaNamespaceKeyFunc(query)
+			if err == nil {
+				op.queues.reportGenerationQueryQueue.Add(key)
 			}
 		}
-		return false, nil
-	}
 
-	if uninitializedDataSources := op.getUnitilizedDataSources(dataSources); len(uninitializedDataSources) > 0 {
-		dataSourcesStr := strings.Join(uninitializedDataSources, ", ")
-		logger.Warnf("the following datasources for the query do not have their tables created %s", dataSourcesStr)
-		if queueUninitialized {
-			logger.Debugf("queueing uninitializedDataSources: %s", dataSourcesStr)
-			for _, dataSource := range uninitializedDataSources {
-				key, err := cache.MetaNamespaceKeyFunc(dataSource)
-				if err == nil {
-					op.queues.reportDataSourceQueue.Add(key)
-				}
+		for _, dataSource := range dependencyStatus.UninitializedReportDataSources {
+			key, err := cache.MetaNamespaceKeyFunc(dataSource)
+			if err == nil {
+				op.queues.reportDataSourceQueue.Add(key)
 			}
 		}
-		return false, nil
-	}
-	return true, nil
-}
-
-func (op *Reporting) getUnitilizedDataSources(dataSources []*cbTypes.ReportDataSource) []string {
-	var uninitializedDataSources []string
-	for _, dataSource := range dataSources {
-		if dataSource.TableName == "" {
-			uninitializedDataSources = append(uninitializedDataSources, dataSource.Name)
-		}
-	}
-	return uninitializedDataSources
-}
-
-func (op *Reporting) getUninitializedReportGenerationQueries(generationQueries []*cbTypes.ReportGenerationQuery) ([]string, error) {
-	var uninitializedQueries, queriesWithDisabledView []string
-	for _, query := range generationQueries {
-		if query.ViewName == "" {
-			if query.Spec.View.Disabled {
-				queriesWithDisabledView = append(queriesWithDisabledView, query.Name)
-			} ***REMOVED*** {
-				uninitializedQueries = append(uninitializedQueries, query.Name)
-			}
-		}
-	}
-
-	if len(queriesWithDisabledView) > 0 {
-		return nil, fmt.Errorf("invalid ReportGenerationQuery, references ReportGenerationQueries with spec.view.disabled=true: %s", strings.Join(queriesWithDisabledView, ", "))
-	}
-	return uninitializedQueries, nil
-}
-
-func (op *Reporting) getDependentGenerationQueries(generationQuery *cbTypes.ReportGenerationQuery, dynamicQueries bool) ([]*cbTypes.ReportGenerationQuery, error) {
-	queriesAccumulator := make(map[string]*cbTypes.ReportGenerationQuery)
-	const maxDepth = 100
-	err := op.getDependentGenerationQueriesMemoized(generationQuery, 0, maxDepth, queriesAccumulator, dynamicQueries)
-	if err != nil {
 		return nil, err
 	}
-	queries := make([]*cbTypes.ReportGenerationQuery, 0, len(queriesAccumulator))
-	for _, query := range queriesAccumulator {
-		queries = append(queries, query)
-	}
-	return queries, nil
-}
-
-func (op *Reporting) getDependentGenerationQueriesMemoized(generationQuery *cbTypes.ReportGenerationQuery, depth, maxDepth int, queriesAccumulator map[string]*cbTypes.ReportGenerationQuery, dynamicQueries bool) error {
-	if depth >= maxDepth {
-		return fmt.Errorf("detected a cycle at depth %d for generationQuery %s", depth, generationQuery.Name)
-	}
-	var queries []string
-	if dynamicQueries {
-		queries = generationQuery.Spec.DynamicReportQueries
-	} ***REMOVED*** {
-		queries = generationQuery.Spec.ReportQueries
-	}
-	for _, queryName := range queries {
-		if _, exists := queriesAccumulator[queryName]; exists {
-			continue
-		}
-		genQuery, err := op.informers.Metering().V1alpha1().ReportGenerationQueries().Lister().ReportGenerationQueries(op.cfg.Namespace).Get(queryName)
-		if err != nil {
-			return err
-		}
-		err = op.getDependentGenerationQueriesMemoized(genQuery, depth+1, maxDepth, queriesAccumulator, dynamicQueries)
-		if err != nil {
-			return err
-		}
-		queriesAccumulator[genQuery.Name] = genQuery
-	}
-	return nil
-}
-
-func (op *Reporting) getDependentDataSources(generationQuery *cbTypes.ReportGenerationQuery) ([]*cbTypes.ReportDataSource, error) {
-	dataSources := make([]*cbTypes.ReportDataSource, len(generationQuery.Spec.DataSources))
-	for i, dataSourceName := range generationQuery.Spec.DataSources {
-		dataSource, err := op.informers.Metering().V1alpha1().ReportDataSources().Lister().ReportDataSources(op.cfg.Namespace).Get(dataSourceName)
-		if err != nil {
-			return nil, err
-		}
-		dataSources[i] = dataSource
-	}
-	return dataSources, nil
+	return deps, nil
 }
