3adac91dd9e1da9b4832fceb8a08dd3f8a8a31be
Chance Zibolski <czibolsk@redhat.com>
2018-11-06 18:22:03 -0800

    jenkins: Break out openshift and gke into separate e2e/integration jobs

---
+++ jenkins/openshift-e2e.groovy
@@ -0,0 +1,9 @@
+def testRunner = evaluate(readTrusted('jenkins/vars/testRunner.groovy'))
+
+testRunner {
+    testScript = "hack/e2e-ci.sh"
+    testType   = "e2e"
+    kubecon***REMOVED***gCredentialsID = 'openshift-metering-ci-kubecon***REMOVED***g'
+    deployPlatform = "openshift"
+    meteringHttpsAPI = true
+}
