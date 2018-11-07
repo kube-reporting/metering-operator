6f15d362d55f12a9bdb40ebb67d969259b96e12f
Chance Zibolski <czibolsk@redhat.com>
2018-11-06 20:33:50 -0800

    Update test runner to use directories for dividing up files
    Also updates test-runner script to have default filenames

--- hack/e2e-test-runner.sh
+++ hack/e2e-test-runner.sh
@@ -6,12 +6,11 @@ set -e
 
 : "${TEST_SCRIPT:?}"
 
-: "${TEST_LOG_FILE:?}"
-: "${DEPLOY_LOG_FILE:?}"
-: "${TEST_TAP_FILE:?}"
-
-: "${DEPLOY_POD_LOGS_LOG_FILE:=""}"
-: "${FINAL_POD_LOGS_LOG_FILE:=""}"
+: "${TEST_TAP_FILE:=tests.tap}"
+: "${TEST_LOG_FILE:=tests.txt}"
+: "${DEPLOY_LOG_FILE:=deploy.log}"
+: "${DEPLOY_POD_LOGS_LOG_FILE:=pod-logs.log}"
+: "${FINAL_POD_LOGS_LOG_FILE:=final-pod-descriptions-logs.log}"
 
 : "${DEPLOY_METERING:=true}"
 : "${TEST_METERING:=true}"
@@ -37,33 +36,44 @@ echo "\$KUBECONFIG=$KUBECONFIG"
 echo "\$METERING_NAMESPACE=$METERING_NAMESPACE"
 echo "\$INSTALL_METHOD=$INSTALL_METHOD"
 
-mkdir -p $TEST_OUTPUT_PATH
-touch "$TEST_OUTPUT_PATH/$TEST_LOG_FILE"
-touch "$TEST_OUTPUT_PATH/$DEPLOY_LOG_FILE"
-touch "$TEST_OUTPUT_PATH/$TEST_TAP_FILE"
-touch "$TEST_OUTPUT_PATH/$DEPLOY_POD_LOGS_LOG_FILE"
-touch "$TEST_OUTPUT_PATH/$FINAL_POD_LOGS_LOG_FILE"
+REPORTS_DIR=$TEST_OUTPUT_PATH/reports
+LOG_DIR=$TEST_OUTPUT_PATH/logs
+TEST_OUT_DIR=$TEST_OUTPUT_PATH/tests
+
+TEST_LOG_FILE_PATH="${TEST_LOG_FILE_PATH:-$TEST_OUT_DIR/$TEST_LOG_FILE}"
+TEST_TAP_FILE_PATH="${TEST_TAP_FILE_PATH:-$TEST_OUT_DIR/$TEST_TAP_FILE}"
+DEPLOY_LOG_FILE_PATH="${DEPLOY_LOG_FILE_PATH:-$LOG_DIR/$DEPLOY_LOG_FILE}"
+DEPLOY_POD_LOGS_LOG_FILE_PATH="${DEPLOY_POD_LOGS_LOG_FILE_PATH:-$LOG_DIR/$DEPLOY_POD_LOGS_LOG_FILE}"
+FINAL_POD_LOGS_LOG_FILE_PATH="${FINAL_POD_LOGS_LOG_FILE_PATH:-$LOG_DIR/$FINAL_POD_LOGS_LOG_FILE}"
+
+mkdir -p $TEST_OUT_DIR $LOG_DIR $REPORTS_DIR
+touch "$TEST_LOG_FILE_PATH"
+touch "$TEST_TAP_FILE_PATH"
+touch "$DEPLOY_LOG_FILE_PATH"
+touch "$DEPLOY_POD_LOGS_LOG_FILE_PATH"
+touch "$FINAL_POD_LOGS_LOG_FILE_PATH"
 
 # fail with the last non-zero exit code (preserves test fail exit code)
 set -o pipefail
 
 export SKIP_DELETE_CRDS=true
 export DELETE_PVCS=true
+export TEST_RESULT_REPORT_OUTPUT_DIRECTORY="$REPORTS_DIR"
 
 function cleanup() {
     echo "Performing cleanup"
 
     if [ -n "$FINAL_POD_LOGS_LOG_FILE" ]; then
         echo "Capturing final pod logs"
-        capture_pod_logs "$METERING_NAMESPACE" >> "$TEST_OUTPUT_PATH/$FINAL_POD_LOGS_LOG_FILE"
+        capture_pod_logs "$METERING_NAMESPACE" >> "$FINAL_POD_LOGS_LOG_FILE_PATH"
         echo "Finished capturing final pod logs"
     fi
 
     echo "Running uninstall"
     if [ "$OUTPUT_DEPLOY_LOG_STDOUT" == "true" ]; then
-        uninstall_metering "$INSTALL_METHOD" | tee -a "$TEST_OUTPUT_PATH/$DEPLOY_LOG_FILE"
+        uninstall_metering "$INSTALL_METHOD" | tee -a "$DEPLOY_LOG_FILE_PATH"
     else
-        uninstall_metering "$INSTALL_METHOD" >> "$TEST_OUTPUT_PATH/$DEPLOY_LOG_FILE"
+        uninstall_metering "$INSTALL_METHOD" >> "$DEPLOY_LOG_FILE_PATH"
     fi
 
     # kill any background jobs, such as stern
@@ -76,9 +86,9 @@ if [ "$DEPLOY_METERING" == "true" ]; then
     if [ -n "$DEPLOY_POD_LOGS_LOG_FILE" ]; then
         echo "Capturing pod logs"
         if [ "$OUTPUT_POD_LOG_STDOUT" == "true" ]; then
-            stern --timestamps -n "$METERING_NAMESPACE" '.*' | tee -a "$TEST_OUTPUT_PATH/$DEPLOY_POD_LOGS_LOG_FILE" &
+            stern --timestamps -n "$METERING_NAMESPACE" '.*' | tee -a "$DEPLOY_POD_LOGS_LOG_FILE_PATH" &
         else
-            stern --timestamps -n "$METERING_NAMESPACE" '.*' >> "$TEST_OUTPUT_PATH/$DEPLOY_POD_LOGS_LOG_FILE" &
+            stern --timestamps -n "$METERING_NAMESPACE" '.*' >> "$DEPLOY_POD_LOGS_LOG_FILE_PATH" &
         fi
     fi
 
@@ -88,9 +98,9 @@ if [ "$DEPLOY_METERING" == "true" ]; then
 
     echo "Deploying Metering"
     if [ "$OUTPUT_DEPLOY_LOG_STDOUT" == "true" ]; then
-        "$ROOT_DIR/hack/${DEPLOY_SCRIPT}" | tee -a "$TEST_OUTPUT_PATH/$DEPLOY_LOG_FILE" 2>&1
+        "$ROOT_DIR/hack/${DEPLOY_SCRIPT}" | tee -a "$DEPLOY_LOG_FILE_PATH" 2>&1
     else
-        "$ROOT_DIR/hack/${DEPLOY_SCRIPT}" >> "$TEST_OUTPUT_PATH/$DEPLOY_LOG_FILE" 2>&1
+        "$ROOT_DIR/hack/${DEPLOY_SCRIPT}" >> "$DEPLOY_LOG_FILE_PATH" 2>&1
     fi
 fi
 
@@ -99,17 +109,17 @@ if [ "$TEST_METERING" == "true" ]; then
 
 
     if [ "$OUTPUT_TEST_LOG_STDOUT" == "true" ]; then
-        tail -f "$TEST_OUTPUT_PATH/$TEST_LOG_FILE" &
+        tail -f "$TEST_LOG_FILE_PATH" &
     fi
 
     "$TEST_SCRIPT" 2>&1 \
-        | tee -a "$TEST_OUTPUT_PATH/$TEST_LOG_FILE" \
+        | tee -a "$TEST_LOG_FILE_PATH" \
         | "$ROOT_DIR/bin/test2json" \
-        | tee -a "$TEST_OUTPUT_PATH/${TEST_LOG_FILE}.json" \
+        | tee -a "${TEST_LOG_FILE}.json_PATH" \
         | jq -r -s -f "$ROOT_DIR/hack/tap-output.jq" \
-        | tee -a "$TEST_OUTPUT_PATH/$TEST_TAP_FILE"
+        | tee -a "$TEST_TAP_FILE_PATH"
 
-    if grep -q '^not' < "$TEST_OUTPUT_PATH/$TEST_LOG_FILE"; then
+    if grep -q '^not' < "$TEST_LOG_FILE_PATH"; then
       exit 1
     else
       exit 0
--- jenkins/vars/testRunner.groovy
+++ jenkins/vars/testRunner.groovy
@@ -6,7 +6,7 @@ def call(body) {
     body()
 
     def commonPrefix = "${pipelineParams.deployPlatform}-${pipelineParams.testType}"
-    def branchPrefix = "${commonPrefix}-${params.DEPLOY_TAG ?: env.BRANCH_NAME}"
+    def branchPrefix = "${commonPrefix}-${params.DEPLOY_TAG ?: env.BRANCH_NAME.toLowerCase()}"
     def defaultNamespace = "metering-ci2-${branchPrefix}"
     def prStatusContext = "jenkins/${commonPrefix}"
 
@@ -26,7 +26,7 @@ def call(body) {
         agent {
             kubernetes {
                 cloud 'gke-metering'
-                label "gke-operator-metering-${commonPrefix}-${params.DEPLOY_TAG ?: env.BRANCH_NAME}"
+                label "gke-operator-metering-${branchPrefix}"
                 instanceCap 2
                 idleMinutes 0
                 defaultContainer 'jnlp'
@@ -72,11 +72,6 @@ spec:
             // use the OVERRIDE_NAMESPACE if specified, otherwise set namespace to prefix + BRANCH_NAME
             METERING_NAMESPACE          = "${params.OVERRIDE_NAMESPACE ?: defaultNamespace}"
             SCRIPT                      = "${pipelineParams.testScript}"
-            TEST_LOG_FILE               = "${pipelineParams.testType}-tests.log"
-            TEST_TAP_FILE               = "${pipelineParams.testType}-tests.tap"
-            DEPLOY_LOG_FILE             = "${pipelineParams.testType}-deploy.log"
-            DEPLOY_POD_LOGS_LOG_FILE    = "${pipelineParams.testType}-pod.log"
-            FINAL_POD_LOGS_LOG_FILE     = "${pipelineParams.testType}-pod-final-descriptions.log"
             // we set CLEANUP_METERING to false and instead handle cleanup on
             // our own, so that if there's a test timeout, we can still capture
             // pod logs
@@ -87,20 +82,20 @@ spec:
         stages {
             stage('Run Tests') {
                 environment {
-                    KUBECONFIG                          = credentials("${pipelineParams.kubeconfigCredentialsID}")
-                    TEST_OUTPUT_DIR                     = "${env.OUTPUT_DIR}/${commonPrefix}-tests"
-                    TEST_OUTPUT_PATH                    = "${env.WORKSPACE}/${env.TEST_OUTPUT_DIR}"
-                    TEST_RESULT_REPORT_OUTPUT_DIRECTORY = "${env.WORKSPACE}/${env.TEST_OUTPUT_DIR}/reports"
-                    DEPLOY_PLATFORM                     = "${pipelineParams.deployPlatform}"
-                    METERING_HTTPS_API                  = "${pipelineParams.meteringHttpsAPI ?: false}"
+                    KUBECONFIG                   = credentials("${pipelineParams.kubeconfigCredentialsID}")
+                    TEST_OUTPUT_DIR              = "${env.OUTPUT_DIR}/${commonPrefix}-tests"
+                    TEST_OUTPUT_PATH             = "${env.WORKSPACE}/${env.TEST_OUTPUT_DIR}"
+                    FINAL_POD_LOGS_LOG_FILE_PATH = "${env.TEST_OUTPUT_PATH}/logs/final-pod-descriptions-logs.log"
+                    DEPLOY_PLATFORM              = "${pipelineParams.deployPlatform}"
+                    METERING_HTTPS_API           = "${pipelineParams.meteringHttpsAPI ?: false}"
                 }
                 steps {
-                    runTests()
+                    runScript()
                 }
                 post {
                     always {
                         echo 'Capturing test TAP output'
-                        step([$class: "TapPublisher", testResults: "${TEST_OUTPUT_DIR}/${TEST_TAP_FILE}", failIfNoResults: false, planRequired: false])
+                        step([$class: "TapPublisher", testResults: "${TEST_OUTPUT_DIR}/**/*.tap", failIfNoResults: false, planRequired: false])
                     }
                     cleanup {
                         cleanup()
@@ -133,13 +128,12 @@ spec:
     }
 }
 
-private def runTests() {
+private def runScript() {
     container('metering-test-runner') {
         ansiColor('xterm') {
             timeout(15) {
                 sh '''#!/bin/bash -ex
                 cd $METERING_SRC_DIR
-                mkdir -p $TEST_OUTPUT_PATH $TEST_RESULT_REPORT_OUTPUT_DIRECTORY
                 $SCRIPT
                 '''
             }
@@ -150,7 +144,7 @@ private def runTests() {
 private def cleanup() {
     container('metering-test-runner') {
         echo "Capturing pod logs"
-        sh 'set -e; cd $METERING_SRC_DIR && ./hack/capture-pod-logs.sh $METERING_NAMESPACE > $TEST_OUTPUT_PATH/$FINAL_POD_LOGS_LOG_FILE'
+        sh 'set -e; cd $METERING_SRC_DIR && ./hack/capture-pod-logs.sh $METERING_NAMESPACE > $FINAL_POD_LOGS_LOG_FILE_PATH'
         echo "Deleting namespace ${env.METERING_NAMESPACE}"
         if (!params.SKIP_NS_CLEANUP) {
             sh 'set -e; cd $METERING_SRC_DIR && ./hack/delete-ns.sh'
