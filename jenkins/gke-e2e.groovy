94a4025b5095ae4f7ac62f29f6f4aae68bdbbd4a
Chance Zibolski <czibolsk@redhat.com>
2018-11-19 13:06:21 -0800

    jenkins: Remove GKE jobs

--- jenkins/gke-e2e.groovy
+++
@@ -1,8 +0,0 @@
-def testRunner = evaluate(readTrusted('jenkins/vars/testRunner.groovy'))
-
-testRunner {
-    testScript = "hack/e2e-ci.sh"
-    testType   = "e2e"
-    kubeconfigCredentialsID = 'gke-metering-ci-kubeconfig'
-    deployPlatform = "generic"
-}
