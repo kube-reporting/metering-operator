d8594bae91b6e677c415a80be40905c05912e856
Chance Zibolski <czibolsk@redhat.com>
2018-11-20 12:47:45 -0800

    hack: Default to using HTTPS reporting api in integration/e2e scripts

--- hack/run-integration-tests.sh
+++ hack/run-integration-tests.sh
@@ -3,7 +3,7 @@ set -e
 
 export METERING_NAMESPACE=${METERING_NAMESPACE:-metering-ci-integration}
 export METERING_SHORT_TESTS=${METERING_SHORT_TESTS:-false}
-export METERING_HTTPS_API=${METERING_HTTPS_API:-false}
+export METERING_HTTPS_API=${METERING_HTTPS_API:-true}
 
 ROOT_DIR=$(dirname "${BASH_SOURCE}")/..
 source "${ROOT_DIR}/hack/common.sh"
