0ae9985cd02ac2ca13919a5b84d3e02065cb204c
Chance Zibolski <czibolsk@redhat.com>
2018-12-20 10:49:18 -0800

    hack,charts: Add chart for doing metering CR configuration for CI jobs

--- hack/deploy-e2e.sh
+++ hack/deploy-e2e.sh
@@ -2,105 +2,72 @@
 set -e
 
 DIR=$(dirname "${BASH_SOURCE}")
+ROOT_DIR="$DIR/.."
 
 : "${DEPLOY_TAG:?}"
+
+TMP_DIR="$(mktemp -d)"
 
 # Used in deploy.sh
 export DOCKER_USERNAME="$DOCKER_CREDS_USR"
 export DOCKER_PASSWORD="$DOCKER_CREDS_PSW"
 
-export DISABLE_PROMSUM=true
-export METERING_CREATE_PULL_SECRET="${METERING_CREATE_PULL_SECRET:=true}"
-
 export CUSTOM_METERING_CR_FILE="$TMP_DIR/custom-metering-cr-${DEPLOY_TAG}.yaml"
 export CUSTOM_HELM_OPERATOR_OVERRIDE_VALUES=${CUSTOM_HELM_OPERATOR_OVERRIDE_VALUES:-"$TMP_DIR/custom-helm-operator-values-${DEPLOY_TAG}.yaml"}
 export CUSTOM_ALM_OVERRIDE_VALUES=${CUSTOM_ALM_OVERRIDE_VALUES:-"$TMP_DIR/custom-alm-values-${DEPLOY_TAG}.yaml"}
 
+export METERING_PULL_SECRET_NAME
+export METERING_CREATE_PULL_SECRET
+
 : "${ENABLE_AWS_BILLING:=false}"
-: "${DISABLE_PROMSUM:=false}"
+: "${DISABLE_PROMSUM:=true}"
 : "${AWS_ACCESS_KEY_ID:=}"
 : "${AWS_SECRET_ACCESS_KEY:=}"
 : "${AWS_BILLING_BUCKET:=}"
 : "${AWS_BILLING_BUCKET_PREFIX:=}"
 : "${AWS_BILLING_BUCKET_REGION:=}"
-: "${METERING_CREATE_PULL_SECRET:=false}"
+: "${METERING_CREATE_PULL_SECRET:=true}"
 : "${METERING_PULL_SECRET_NAME:=metering-pull-secret}"
 : "${TERMINATION_GRACE_PERIOD_SECONDS:=0}"
-
 : "${HDFS_NAMENODE_STORAGE_SIZE:=5Gi}"
 : "${HDFS_DATANODE_STORAGE_SIZE:=5Gi}"
+: "${CUR_DATE:=$(date +%s)}"
 
-IMAGE_PULL_SECRET_TEXT=""
+HELM_ARGS=(\
+    --set "enableAwsBilling=$ENABLE_AWS_BILLING" \
+    --set "disablePromsum=${DISABLE_PROMSUM}" \
+    --set "awsAccessKeyId=${AWS_ACCESS_KEY_ID}" \
+    --set "awsSecretAccessKey=${AWS_SECRET_ACCESS_KEY}" \
+    --set "awsBillingBucket=${AWS_BILLING_BUCKET}" \
+    --set "awsBillingBucketPre***REMOVED***x=${AWS_BILLING_BUCKET_PREFIX}" \
+    --set "awsBillingBucketRegion=${AWS_BILLING_BUCKET_REGION}" \
+    --set "meteringPullSecretName=${METERING_PULL_SECRET_NAME}" \
+    --set "terminationGracePeriodSeconds=${TERMINATION_GRACE_PERIOD_SECONDS}" \
+    --set "hdfsNamenodeStorageSize=${HDFS_NAMENODE_STORAGE_SIZE}" \
+    --set "hdfsDatanodeStorageSize=${HDFS_DATANODE_STORAGE_SIZE}" \
+    --set "dateAnnotationValue=currdate-$CUR_DATE" \
+)
+
 if [ "$METERING_CREATE_PULL_SECRET" == "true" ]; then
-    IMAGE_PULL_SECRET_TEXT="imagePullSecrets: [ { name: \"$METERING_PULL_SECRET_NAME\" } ]"
+    HELM_ARGS+=(--set "imagePullSecretName=$METERING_PULL_SECRET_NAME")
 ***REMOVED***
 
-CUR_DATE="$(date +%s)"
-DATE_ANNOTATION="\"metering.deploy-custom/deploy-time\": \"$CUR_DATE\""
+helm template \
+    "$ROOT_DIR/charts/metering-ci" \
+    -x templates/metering.yaml \
+    "${HELM_ARGS[@]}" \
+    | sed -f "$ROOT_DIR/hack/remove-helm-template-header.sed" \
+    > "$CUSTOM_METERING_CR_FILE"
 
-cat <<EOF > "$CUSTOM_METERING_CR_FILE"
-apiVersion: metering.openshift.io/v1alpha1
-kind: Metering
-metadata:
-  name: "${DEPLOY_PLATFORM}-metering"
-spec:
-  openshift-reporting:
-    spec:
-      awsBillingReportDataSource:
-        enabled: ${ENABLE_AWS_BILLING}
-        bucket: "${AWS_BILLING_BUCKET}"
-        pre***REMOVED***x: "${AWS_BILLING_BUCKET_PREFIX}"
-        region: "${AWS_BILLING_BUCKET_REGION}"
-
-  reporting-operator:
-    spec:
-      image:
-        tag: ${DEPLOY_TAG}
-
-      ${IMAGE_PULL_SECRET_TEXT:-}
-      annotations: { $DATE_ANNOTATION }
-      terminationGracePeriodSeconds: ${TERMINATION_GRACE_PERIOD_SECONDS}
-
-      con***REMOVED***g:
-        disablePromsum: ${DISABLE_PROMSUM}
-        awsAccessKeyID: "${AWS_ACCESS_KEY_ID}"
-        awsSecretAccessKey: "${AWS_SECRET_ACCESS_KEY}"
-
-
-  presto:
-    spec:
-      ${IMAGE_PULL_SECRET_TEXT:-}
-      con***REMOVED***g:
-        awsAccessKeyID: "${AWS_ACCESS_KEY_ID}"
-        awsSecretAccessKey: "${AWS_SECRET_ACCESS_KEY}"
-      presto:
-        annotations: { $DATE_ANNOTATION }
-        terminationGracePeriodSeconds: ${TERMINATION_GRACE_PERIOD_SECONDS}
-      hive:
-        annotations: { $DATE_ANNOTATION }
-        terminationGracePeriodSeconds: ${TERMINATION_GRACE_PERIOD_SECONDS}
-
-  hdfs:
-    spec:
-      ${IMAGE_PULL_SECRET_TEXT:-}
-      datanode:
-        annotations: { $DATE_ANNOTATION }
-        terminationGracePeriodSeconds: ${TERMINATION_GRACE_PERIOD_SECONDS}
-        storage:
-          size: ${HDFS_DATANODE_STORAGE_SIZE}
-      namenode:
-        annotations: { $DATE_ANNOTATION }
-        terminationGracePeriodSeconds: ${TERMINATION_GRACE_PERIOD_SECONDS}
-        storage:
-          size: ${HDFS_NAMENODE_STORAGE_SIZE}
-EOF
+# use the CUSTOM_METERING_CR_FILE as the CR values for the helm-operator chart values below
+CR_SPEC=$("$ROOT_DIR/hack/yamltojson" < "$CUSTOM_METERING_CR_FILE" | jq -r '{ cr: {spec: .spec} }' | "$ROOT_DIR/hack/jsontoyaml")
 
 cat <<EOF > "$CUSTOM_HELM_OPERATOR_OVERRIDE_VALUES"
 image:
   tag: ${DEPLOY_TAG}
 annotations: { $DATE_ANNOTATION }
 reconcileIntervalSeconds: 5
-${IMAGE_PULL_SECRET_TEXT:-}
+${CR_SPEC}
 EOF
 
 cat <<EOF > "$CUSTOM_ALM_OVERRIDE_VALUES"
@@ -113,7 +80,5 @@ spec:
   matchLabels:
     alm-owner-metering: metering-operator
 EOF
-
-export METERING_CR_FILE="$CUSTOM_METERING_CR_FILE"
 
 "$DIR/deploy-custom.sh"
