0b66949e260d5bb8dfcfa05aef5efe26c45e86e4
Chance Zibolski <czibolsk@redhat.com>
2019-03-01 14:32:21 -0800

    hack: improve capturing pod logs in e2e tests

--- hack/capture-pod-logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-ROOT_DIR=$(dirname "${BASH_SOURCE[0]}")/..
-source "${ROOT_DIR}/hack/common.sh"
-
-: "${1:?Must be the namespace which pod\'s logs you want to capture}"
-
-capture_pod_logs "$1"
