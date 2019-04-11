fdcea227b6e5f5734bcef706ddfe4ad2b1b6bf75
Chance Zibolski <czibolsk@redhat.com>
2019-04-11 14:01:08 -0700

    test: Improve test output on failure to store data

--- test/framework/db.go
+++ test/framework/db.go
@@ -4,14 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"testing"
 
 	"github.com/operator-framework/operator-metering/pkg/operator"
 	"github.com/operator-framework/operator-metering/pkg/operator/prestostore"
-	"github.com/stretchr/testify/require"
 )
 
-func (f *Framework) StoreDataSourceData(t *testing.T, dataSourceName string, metrics []*prestostore.PrometheusMetric) error {
+func (f *Framework) StoreDataSourceData(dataSourceName string, metrics []*prestostore.PrometheusMetric) error {
 	params := operator.StorePromsumDataRequest(metrics)
 	body, err := json.Marshal(params)
 	if err != nil {
@@ -19,11 +17,13 @@ func (f *Framework) StoreDataSourceData(t *testing.T, dataSourceName string, met
 	}
 
 	url := fmt.Sprintf("/api/v1/datasources/prometheus/store/%s/%s", f.Namespace, dataSourceName)
-	_, respCode, err := f.ReportingOperatorPOSTRequest(url, body)
+	respBody, respCode, err := f.ReportingOperatorPOSTRequest(url, body)
 	if err != nil {
 		return fmt.Errorf("error storing datasource data: %s", err)
 	}
-	require.Equal(t, http.StatusOK, respCode, "http response status code should be ok")
+	if respCode != http.StatusOK {
+		return fmt.Errorf("got http status %d when storing Metrics for ReportDataSource %s, body: %s", respCode, dataSourceName, string(respBody))
+	}
 
 	return nil
 }
