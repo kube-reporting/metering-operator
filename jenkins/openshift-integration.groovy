bef65a9cc5c3436f45460686557dbb3452020fe4
Chance Zibolski <czibolsk@redhat.com>
2019-04-15 13:52:12 -0700

    *: Remove jenkins ci code and documentation

--- jenkins/openshift-integration.groovy
+++
@@ -1,9 +0,0 @@
-def testRunner = evaluate(readTrusted('jenkins/vars/testRunner.groovy'))
-
-testRunner {
-    testScript = "hack/integration.sh"
-    testType   = "integration"
-    kubecon***REMOVED***gCredentialsID = 'openshift-metering-ci-kubecon***REMOVED***g'
-    deployPlatform = "openshift"
-    meteringHttpsAPI = true
-}
