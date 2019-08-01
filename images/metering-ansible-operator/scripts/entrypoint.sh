af6754df87855463724eb9bddef05fe35a4decf8
Chance Zibolski <czibolsk@redhat.com>
2019-08-01 13:37:10 -0700

    metering-ansible-operator: Move tini into $PATH and update entrypoint
    Use `tini` from path instead of `/tini` because the OCP image installs
    tini from a package and it ends up in /usr/bin/tini.

--- images/metering-ansible-operator/scripts/entrypoint.sh
+++ images/metering-ansible-operator/scripts/entrypoint.sh
@@ -17,4 +17,5 @@ if ! whoami &> /dev/null; then
     fi
 fi
 
-exec /tini -- /usr/local/bin/ansible-operator run ansible --watches-file=/opt/ansible/watches.yaml "$@"
+# we expect tini to be in the $PATH
+exec tini -- /usr/local/bin/ansible-operator run ansible --watches-file=/opt/ansible/watches.yaml "$@"
