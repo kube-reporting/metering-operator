3620b823a818cd9653d91095a9bbe09409a7def4
timflannagan1 <timflannagan@gmail.com>
2019-09-18 18:39:31 -0400

    cmd: Update shared flags between commands to be persistent flags under the root command.

--- cmd/deploy-metering/main.go
+++ cmd/deploy-metering/main.go
@@ -52,54 +52,24 @@ var (
 )
 
 func init() {
-	uninstallCmd.Flags().StringVar(&cfg.Namespace, "namespace", "", "The namespace to install the metering resources. This can also be specified through the METERING_NAMESPACE ENV var.")
-	uninstallCmd.Flags().StringVar(&cfg.Platform, "platform", "openshift", "The platform to install the metering stack on. Supported options are 'openshift', 'upstream', or 'ocp-testing'. This can also be specified through the DEPLOY_PLATFORM ENV var.")
-	uninstallCmd.Flags().StringVar(&cfg.MeteringCR, "meteringconfig", "", "The absolute/relative path to the MeteringConfig custom resource. This can also be specified through the METERING_CR_FILE ENV var")
-	uninstallCmd.Flags().StringVar(&cfg.ManifestLocation, "manifest-dir", "", "The absolute/relative path to the metering manfiest directory. This can also be specified through the INSTALLER_MANIFESTS_DIR")
+	rootCmd.PersistentFlags().StringVar(&cfg.Namespace, "namespace", "", "The namespace to install the metering resources. This can also be specified through the METERING_NAMESPACE ENV var.")
+	rootCmd.PersistentFlags().StringVar(&cfg.Platform, "platform", "openshift", "The platform to install the metering stack on. Supported options are 'openshift', 'upstream', or 'ocp-testing'. This can also be specified through the DEPLOY_PLATFORM ENV var.")
+	rootCmd.PersistentFlags().StringVar(&cfg.MeteringCR, "meteringconfig", "", "The absolute/relative path to the MeteringConfig custom resource. This can also be specified through the METERING_CR_FILE ENV var.")
+	rootCmd.PersistentFlags().StringVar(&cfg.DeployManifestsDirectory, "deploy-manifests-dir", "manifests/deploy", "The absolute/relative path to the metering manifest directory. This can also be specified through the INSTALLER_MANIFESTS_DIR.")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", log.DebugLevel.String(), "The logging level when deploying metering")
+
 	uninstallCmd.Flags().BoolVar(&cfg.DeleteCRDs, "delete-crd", false, "If true, this would delete the metering CRDs during an uninstall. This can also be specified through the METERING_DELETE_CRDS ENV var.")
 	uninstallCmd.Flags().BoolVar(&cfg.DeleteCRB, "delete-crb", false, "If true, this would delete the metering cluster role bindings during an uninstall. This can also be specified through METERING_DELETE_CRB ENV var.")
 	uninstallCmd.Flags().BoolVar(&cfg.DeleteNamespace, "delete-namespace", false, "If true, this would delete the namespace during an uninstall. This can also be specified through the METERING_DELETE_NAMESPACE ENV var.")
 	uninstallCmd.Flags().BoolVar(&cfg.DeletePVCs, "delete-pvc", true, "If true, this would delete the PVCs used by metering resources during an uninstall. This can also be specified through the METERING_DELETE_PVCS ENV var.")
 	uninstallCmd.Flags().BoolVar(&cfg.DeleteAll, "delete-all", false, "If true, this would delete the all metering resources during an uninstall. This can also be specified through the METERING_DELETE_ALL ENV var.")
 
-	installCmd.Flags().StringVar(&cfg.Namespace, "namespace", "", "The namespace to install the metering resources. This can also be specified through the METERING_NAMESPACE ENV var.")
-	installCmd.Flags().StringVar(&cfg.Platform, "platform", "openshift", "The platform to install the metering stack on. Supported options are 'openshift', 'upstream', or 'ocp-testing'. This can also be specified through the DEPLOY_PLATFORM ENV var.")
-	installCmd.Flags().StringVar(&cfg.MeteringCR, "meteringconfig", "", "The absolute/relative path to the MeteringConfig custom resource. This can also be specified through the METERING_CR_FILE ENV var")
-	installCmd.Flags().StringVar(&cfg.DeployManifestsDirectory, "deploy-manifests-dir", "manifests/deploy", "The absolute/relative path to the metering manfiest directory. This can also be specified through the INSTALLER_MANIFESTS_DIR")
-	installCmd.Flags().StringVar(&cfg.Repo, "repo", "", "The name of the metering-ansible-operator image repository. This can also be specified through the METERING_OPERATOR_IMAGE_REPO ENV var")
-	installCmd.Flags().StringVar(&cfg.Tag, "tag", "", "The name of the metering-ansible-operator image tag. This can also be specified through the METERING_OPERATOR_IMAGE_TAG ENV var")
+	installCmd.Flags().StringVar(&cfg.Repo, "repo", "", "The name of the metering-ansible-operator image repository. This can also be specified through the METERING_OPERATOR_IMAGE_REPO ENV var.")
+	installCmd.Flags().StringVar(&cfg.Tag, "tag", "", "The name of the metering-ansible-operator image tag. This can also be specified through the METERING_OPERATOR_IMAGE_TAG ENV var.")
 	installCmd.Flags().BoolVar(&cfg.SkipMeteringDeployment, "skip-metering-operator-deployment", false, "If true, only create the metering namespace, CRDs, and MeteringConfig resources. This can also be specified through the SKIP_METERING_OPERATOR_DEPLOY ENV var.")
 
-	installEnv := map[string]string{
-		"METERING_NAMESPACE":                "namespace",
-		"DEPLOY_PLATFORM":                   "platform",
-		"METERING_CR_FILE":                  "meteringconfig",
-		"SKIP_METERING_OPERATOR_DEPLOYMENT": "skip-metering-operator-deployment",
-		"DEPLOY_MANIFESTS_DIR":              "deploy-manifests-dir",
-		"METERING_OPERATOR_IMAGE_REPO":      "repo",
-		"METERING_OPERATOR_IMAGE_TAG":       "tag",
-	}
-
-	err := mapEnvVarToFlag(installEnv, installCmd.Flags())
-	if err != nil {
+	if err := initFlagsFromEnv(); err != nil {
 		log.WithError(err).Fatalf("Failed to update flags from ENV vars: %v", err)
-	}
-
-	uninstallEnv := map[string]string{
-		"METERING_NAMESPACE":        "namespace",
-		"DEPLOY_PLATFORM":           "platform",
-		"METERING_CR_FILE":          "meteringconfig",
-		"DEPLOY_MANIFESTS_DIR":      "deploy-manifests-dir",
-		"METERING_DELETE_CRB":       "delete-crb",
-		"METERING_DELETE_CRDS":      "delete-crd",
-		"METERING_DELETE_PVCS":      "delete-pvc",
-		"METERING_DELETE_NAMESPACE": "delete-namespace",
-		"METERING_DELETE_ALL":       "delete-all",
-	}
-
-	err = mapEnvVarToFlag(uninstallEnv, uninstallCmd.Flags())
-	if err != nil {
-		log.WithError(err).Fatalf("Failed to update flags from ENV vars (uninstall): %v", err)
 	}
 }
 
@@ -185,6 +155,56 @@ func setupLogger(logLevelStr string) log.FieldLogger {
 	logger.Logger.Level = logLevel
 
 	return logger
+}
+
+func initFlagsFromEnv() error {
+	flagEnvConf := []struct {
+		cmd *cobra.Command
+		env map[string]string
+	}{
+		{
+			cmd: rootCmd,
+			env: map[string]string{
+				"METERING_NAMESPACE":        "namespace",
+				"DEPLOY_PLATFORM":           "platform",
+				"METERING_CR_FILE":          "meteringconfig",
+				"DEPLOY_MANIFESTS_DIR":      "deploy-manifests-dir",
+				"METERING_DEPLOY_LOG_LEVEL": "log-level",
+			},
+		},
+		{
+			cmd: uninstallCmd,
+			env: map[string]string{
+				"METERING_DELETE_CRB":       "delete-crb",
+				"METERING_DELETE_CRDS":      "delete-crd",
+				"METERING_DELETE_PVCS":      "delete-pvc",
+				"METERING_DELETE_NAMESPACE": "delete-namespace",
+				"METERING_DELETE_ALL":       "delete-all",
+			},
+		},
+		{
+			cmd: installCmd,
+			env: map[string]string{
+				"SKIP_METERING_OPERATOR_DEPLOYMENT": "skip-metering-operator-deployment",
+				"METERING_OPERATOR_IMAGE_REPO":      "repo",
+				"METERING_OPERATOR_IMAGE_TAG":       "tag",
+			},
+		},
+	}
+
+	for _, flagConf := range flagEnvConf {
+		flagSet := flagConf.cmd.Flags()
+
+		if flagConf.cmd.HasPersistentFlags() {
+			flagSet = flagConf.cmd.PersistentFlags()
+		}
+
+		if err := mapEnvVarToFlag(flagConf.env, flagSet); err != nil {
+			log.WithError(err).Fatalf("Failed to update flags from ENV vars (%s): %v", flagConf.cmd.Name(), err)
+		}
+	}
+
+	return nil
 }
 
 // mapEnvVarToFlag takes a mapping of ENV var names to flag names and iterates
