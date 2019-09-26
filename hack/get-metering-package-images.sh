cfe467f42d1c8f13ee80202333888a33c3040a9e
Chance Zibolski <czibolsk@redhat.com>
2019-09-26 11:44:30 -0700

    *: Update manifests, scripts and docs to use 4.3

--- hack/get-metering-package-images.sh
+++ hack/get-metering-package-images.sh
@@ -2,4 +2,6 @@
 
 # kubectl -n openshift-marketplace port-forward svc/art-applications 50051
 
-grpcurl -plaintext -d '{"pkgName": "metering-ocp", "channelName": "4.2"}' localhost:50051 api.Registry/GetBundleForChannel  | jq '.csvJson' -r | jq '.spec.install.spec.deployments[0].spec.template.spec.containers[] | select(.name=="operator") | .env | map(select(.name | match(".*_IMAGE"))) | map(.value |= (. | ltrimstr("image-registry.openshift-image-registry.svc:5000/"))) | .[] | "export " + .name + "=" + .value' -r
+CHANNEL="${CHANNEL:-4.3}"
+
+grpcurl -plaintext -d '{"pkgName": "metering-ocp", "channelName": "'"$CHANNEL"'"}' localhost:50051 api.Registry/GetBundleForChannel  | jq '.csvJson' -r | jq '.spec.install.spec.deployments[0].spec.template.spec.containers[] | select(.name=="operator") | .env | map(select(.name | match(".*_IMAGE"))) | map(.value |= (. | ltrimstr("image-registry.openshift-image-registry.svc:5000/"))) | .[] | "export " + .name + "=" + .value' -r
