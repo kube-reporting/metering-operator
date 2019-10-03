454550d9be382856821abab186c038f281b28bdc
Chance Zibolski <czibolsk@redhat.com>
2019-10-03 14:49:12 -0700

    hack: Fix mirroring scripts now that internal image registry is different
    Images from brew are now stored in quay and accessed through an internal
    proxy. As a result, the images are published to a different image
    org than before, and the image names no longer map directly between source and final name.
    So we need to extract the image tag in get-metering-package-images.sh,
    and explicitly set the source image name to the registry proxy image's
    name, and set the output image name to what we normally get.

--- hack/get-metering-package-images.sh
+++ hack/get-metering-package-images.sh
@@ -2,6 +2,6 @@
 
 # kubectl -n openshift-marketplace port-forward svc/art-applications 50051
 
-CHANNEL="${CHANNEL:-4.3}"
+CHANNEL="${CHANNEL:-"4.3"}"
 
-grpcurl -plaintext -d '{"pkgName": "metering-ocp", "channelName": "'"$CHANNEL"'"}' localhost:50051 api.Registry/GetBundleForChannel  | jq '.csvJson' -r | jq '.spec.install.spec.deployments[0].spec.template.spec.containers[] | select(.name=="operator") | .env | map(select(.name | match(".*_IMAGE"))) | map(.value |= (. | ltrimstr("image-registry.openshift-image-registry.svc:5000/"))) | .[] | "export " + .name + "=" + .value' -r
+grpcurl -plaintext -d '{"pkgName": "metering-ocp", "channelName": "'"$CHANNEL"'"}' localhost:50051 api.Registry/GetBundleForChannel  | jq '.csvJson' -r | jq '.spec.install.spec.deployments[0].spec.template.spec.containers[] | select(.name=="operator") | .env | map(select(.name | match(".*_IMAGE"))) | map(.value |= ltrimstr("image-registry.openshift-image-registry.svc:5000/")) | map(.value |= (split(":"))[1]) |  map(.name |= gsub("_IMAGE"; "_IMAGE_TAG")) | .[] | "export " + .name + "=" + .value' -r
