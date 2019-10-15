a365cba3f3051ad6de8ece0e9d7cdbc113ec5df8
Chance Zibolski <czibolsk@redhat.com>
2019-10-15 14:53:21 -0700

    hack: Fix mirroring to work for digests

--- hack/get-metering-package-images.sh
+++ hack/get-metering-package-images.sh
@@ -2,4 +2,20 @@
 
 # kubectl -n openshift-marketplace port-forward svc/art-applications 50051
 
-grpcurl -plaintext -d '{"pkgName": "metering-ocp", "channelName": "4.2"}' localhost:50051 api.Registry/GetBundleForChannel  | jq '.csvJson' -r | jq '.spec.install.spec.deployments[0].spec.template.spec.containers[] | select(.name=="operator") | .env | map(select(.name | match(".*_IMAGE"))) | map(.value |= (. | ltrimstr("image-registry.openshift-image-registry.svc:5000/"))) | .[] | "export " + .name + "=" + .value' -r
+CHANNEL="${CHANNEL:-"4.2"}"
+
+read -r -d '' JQ_PROGRAM << EOM
+ .spec.install.spec.deployments[0].spec.template.spec.containers[]
+ | select(.name=="operator")
+ | .env
+ | map(select(.name | match(".*_IMAGE")))
+ # | map(.value |= ltrimstr("image-registry.openshift-image-registry.svc:5000/"))
+ # | map(.value |= (split(":"))[1])
+ # |  map(.name |= gsub("_IMAGE"; "_IMAGE_TAG"))
+ | .[]
+ | "export " + .name + "=" + .value
+EOM
+
+grpcurl -plaintext -d '{"pkgName": "metering-ocp", "channelName": "'"$CHANNEL"'"}' localhost:50051 api.Registry/GetBundleForChannel  \
+    | jq '.csvJson' -r \
+    | jq "$JQ_PROGRAM" -r
