3f895cb2563dc3bb14d88cad3a2401d31d26484f
timflannagan1 <timflannagan@gmail.com>
2020-02-05 17:15:49 -0500

    pkg: Support creating unmanaged PrestoTables from a ReportDataSource.
    This would add the ability to create a ReportDataSource that references an existing table in Presto. Before, you would need to first create an unmanaged PrestoTable that referenced an existing table in Presto, then create a ReportDataSource that referenced the created PrestoTable. With initial support for the presto-prometheus connector being integrated, this change would be needed as Presto would read direcly from Prometheus and store relevant data into tables which ReportDataSources can then reference.

--- pkg/operator/datasources.go
+++ pkg/operator/datasources.go
@@ -3,6 +3,7 @@ package operator
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -403,13 +404,18 @@ func (op *Reporting) handlePrestoTableDataSource(logger log.FieldLogger, dataSou
 	if dataSource.Spec.PrestoTable == nil {
 		return fmt.Errorf("%s is not a PrestoTable ReportDataSource", dataSource.Name)
 	}
-	if dataSource.Spec.PrestoTable.TableRef.Name == "" {
-		return fmt.Errorf("invalid PrestoTable ReportDataSource %s, spec.prestoTable.tableRef.name must be set", dataSource.Name)
+	if dataSource.Spec.PrestoTable.TableName == "" && dataSource.Spec.PrestoTable.TableRef.Name == "" {
+		return fmt.Errorf("invalid PrestoTable ReportDataSource %s, spec.prestoTable was improperly configured", dataSource.Name)
 	}
 
-	var prestoTable *metering.PrestoTable
+	var (
+		prestoTable     *metering.PrestoTable
+		prestoTableName string
+		err             error
+	)
+	// check if the reporting-operator has already processed this ReportDatasource before
+	// if true: verify the PrestoTable.Name listed in data source's status exists, and exit early
 	if dataSource.Status.TableRef.Name != "" {
-		var err error
 		prestoTable, err = op.prestoTableLister.PrestoTables(dataSource.Namespace).Get(dataSource.Status.TableRef.Name)
 		if err != nil {
 			return fmt.Errorf("unable to get PrestoTable %s for ReportDataSource %s, %s", dataSource.Status.TableRef, dataSource.Name, err)
@@ -418,24 +424,64 @@ func (op *Reporting) handlePrestoTableDataSource(logger log.FieldLogger, dataSou
 		if err != nil {
 			return err
 		}
+
 		logger.Infof("existing PrestoTable ReportDataSource discovered, tableName: %s", tableName)
+		return nil
+	}
+
+	if dataSource.Spec.PrestoTable.TableRef.Name != "" {
+		prestoTableName = dataSource.Spec.PrestoTable.TableRef.Name
 	} else {
-		logger.Infof("new PrestoTable ReportDataSource discovered, tableName: %s", dataSource.Spec.PrestoTable.TableRef.Name)
-		var err error
-		prestoTable, err = op.waitForPrestoTable(dataSource.Namespace, dataSource.Spec.PrestoTable.TableRef.Name, time.Second, 10*time.Second)
+		// we expect the tableName field to be in the form of <catalog>.<schema>.<table_name>
+		// format, so split the string into components and verify the length of the
+		// array is as expected
+		meta := strings.Split(dataSource.Spec.PrestoTable.TableName, ".")
+
+		if len(meta) != 3 {
+			return fmt.Errorf("spec.PrestoTable.TableName is not in the format of a fully-qualified table name")
+		}
+
+		catalog := meta[0]
+		schema := meta[1]
+		tableName := meta[2]
+
+		// check if we can discover the table's column information dynamically and
+		// in the case where the table doesn't exist in Presto, the presto go-client
+		// will return an error describing a non-existent table
+		cols, err := op.prestoTableManager.QueryMetadata(catalog, schema, tableName)
 		if err != nil {
-			return fmt.Errorf("error creating table for ReportDataSource %s: %s", dataSource.Name, err)
+			return fmt.Errorf("failed to successfully discover the %s Presto table metadata: %v", dataSource.Spec.PrestoTable.TableName, err)
 		}
 
 		dsClient := op.meteringClient.MeteringV1().ReportDataSources(dataSource.Namespace)
 		_, err = updateReportDataSource(dsClient, dataSource.Name, func(newDS *metering.ReportDataSource) {
 			newDS.Status.TableRef = v1.LocalObjectReference{Name: prestoTable.Name}
 		})
+		prestoTable, err := op.createPrestoTableCR(dataSource, metering.ReportDataSourceGVK, catalog, schema, tableName, cols, true, false, "")
 		if err != nil {
-			logger.WithError(err).Errorf("failed to update ReportDataSource status.tableRef field %q", prestoTable.Name)
-			return err
+			return fmt.Errorf("failed to create a PrestoTable custom resource for the %s ReportDataSource: %v", dataSource.Name, err)
 		}
+
+		prestoTableName = prestoTable.Name
 	}
+
+	prestoTable, err = op.waitForPrestoTable(dataSource.Namespace, prestoTableName, time.Second, 10*time.Second)
+	if err != nil {
+		return fmt.Errorf("failed to process the %s PrestoTable for the %s ReportDataSource: %v", prestoTableName, dataSource.Name, err)
+	}
+
+	logger.Infof("created the %s PrestoTable from the %s ReportDataSource", prestoTableName, dataSource.Name)
+
+	dsClient := op.meteringClient.MeteringV1().ReportDataSources(dataSource.Namespace)
+	status, err := updateReportDataSource(dsClient, dataSource.Name, func(newDS *metering.ReportDataSource) {
+		newDS.Status.TableRef = v1.LocalObjectReference{Name: prestoTable.Name}
+	})
+	if err != nil {
+		logger.WithError(err).Errorf("failed to update the %s ReportDataSource status.tableRef field to the %s PrestoTable. Here is the datasource object: %+v", dataSource.Name, prestoTable.Name, dataSource.Spec.PrestoTable)
+		return err
+	}
+
+	dataSource.Status = status.Status
 
 	return nil
 }
