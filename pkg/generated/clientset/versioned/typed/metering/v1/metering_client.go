adcaaa3d8fa5fe5e361089dad68ef320772ff73d
origin-release-container <origin-release@redhat.com>
2020-03-30 22:29:42 +0000

    Result of make update-codegen

--- pkg/generated/clientset/versioned/typed/metering/v1/metering_client.go
+++ pkg/generated/clientset/versioned/typed/metering/v1/metering_client.go
@@ -5,7 +5,6 @@ package v1
 import (
 	v1 "github.com/operator-framework/operator-metering/pkg/apis/metering/v1"
 	"github.com/operator-framework/operator-metering/pkg/generated/clientset/versioned/scheme"
-	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	rest "k8s.io/client-go/rest"
 )
 
@@ -85,7 +84,7 @@ func setConfigDefaults(config *rest.Config) error {
 	gv := v1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
-	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
+	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
 
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
