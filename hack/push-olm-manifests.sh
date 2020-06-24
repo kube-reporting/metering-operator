f3a2d47ac9b4f585023828a870f4e4ecc1786c5e
timflannagan1 <timflannagan@gmail.com>
2020-06-24 15:04:51 -0400

    hack: Adapt the push-olm-manifests script to push a manifest bundle.
    This is needed as we can no longer use the operator-courier tooling as it doesn't support v1 CRDs, at least as of yet, and it doesn't appear that there's going to be any changes made to fix that validation check.

--- hack/push-olm-manifests.sh
+++ hack/push-olm-manifests.sh
@@ -5,11 +5,10 @@ set -o pipefail
 ROOT_DIR=$(dirname "${BASH_SOURCE}")/..
 source "${ROOT_DIR}/hack/common.sh"
 
-OLM_PACKAGE_ORG="${1:?}"
-OLM_PACKAGE_NAME="${2:?}"
-MANIFEST_BUNDLE="${3:-$OCP_OLM_MANIFESTS_DIR/bundle}"
-OLM_PACKAGE_VERSION="${OLM_PACKAGE_VERSION:-"4.4.0-$(date +'%Y%m%d%H%M%S')"}"
-: "${QUAY_AUTH_TOKEN:?}"
+IMAGE_METERING_ANSIBLE_OPERATOR_REGISTRY="${1:?}"
+MANIFEST_BUNDLE="${2:-manifests/deploy/openshift/olm/bundle}"
+CONTAINER_RUNTIME=${CONTAINER_RUNTIME:-podman}
 
-echo "operator-courier push $MANIFEST_BUNDLE ${OLM_PACKAGE_ORG} ${OLM_PACKAGE_NAME} ${OLM_PACKAGE_VERSION} <ommited>"
-operator-courier push "$MANIFEST_BUNDLE" "${OLM_PACKAGE_ORG}" "${OLM_PACKAGE_NAME}" "${OLM_PACKAGE_VERSION}" "${QUAY_AUTH_TOKEN}"
+echo "Building and pushing the operator manifest bundle to ${IMAGE_METERING_ANSIBLE_OPERATOR_REGISTRY}"
+${CONTAINER_RUNTIME} build -f ${ROOT_DIR}/olm_deploy/Dockerfile.registry -t ${IMAGE_METERING_ANSIBLE_OPERATOR_REGISTRY} --build-arg MANIFEST_BUNDLE=${MANIFEST_BUNDLE} .
+${CONTAINER_RUNTIME} push ${IMAGE_METERING_ANSIBLE_OPERATOR_REGISTRY}
