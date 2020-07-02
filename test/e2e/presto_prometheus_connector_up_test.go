d63a32e77d77ad034b453b7c0bf3579caab0841f
timflannagan1 <timflannagan@gmail.com>
2020-07-02 09:57:48 -0400

    test/e2e: Update the Prometheus connector test to use the REST client.

--- test/e2e/presto_prometheus_connector_up_test.go
+++ test/e2e/presto_prometheus_connector_up_test.go
@@ -1,66 +1,47 @@
 package e2e
 
 import (
-	"bytes"
-	"github.com/kube-reporting/metering-operator/test/reportingframework"
-	"github.com/stretchr/testify/require"
-	"os"
-	"os/exec"
-	"strings"
-
+	"context"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/kube-reporting/metering-operator/test/reportingframework"
+	"github.com/kube-reporting/metering-operator/test/testhelpers"
+)
+
+const (
+	prestoCoordinatorPodName = "presto-coordinator-0"
+	prestoContainerName      = "presto"
 )
 
 func testPrometheusConnectorWorks(t *testing.T, rf *reportingframework.ReportingFramework) {
-	t.Helper()
+	pod, err := rf.KubeClient.CoreV1().Pods(rf.Namespace).Get(context.Background(), prestoCoordinatorPodName, metav1.GetOptions{})
+	require.Nil(t, err, "expected there would be no error querying for the presto-coordinator-0 Pod in the %s namespace", rf.Namespace)
 
-	// query for the presto-coordinator pod name
-	podNameCmd := exec.Command(
-		"kubectl",
-		"-n", rf.Namespace,
-		"get", "pods",
-		"-l", "app=presto,presto=coordinator",
-		"-o", "name",
-	)
+	t.Logf("Found the %s pod in the %s namespace", pod.Name, pod.Namespace)
 
-	var prestoHostResults bytes.Buffer
-	podNameCmd.Stderr = os.Stderr
-	podNameCmd.Stdout = &prestoHostResults
-
-	err := podNameCmd.Run()
-	require.Nil(t, err, "expected querying for the presto pod name would produce no error")
-	require.NotEmpty(t, prestoHostResults.String(), "unable to parse the presto-coordinator pod name")
-
-	t.Logf("querying for the pod/presto-coordinator name returned: %s", prestoHostResults.String())
-
-	// we know the output is going to be of the form `pod/<presto coordinator pod name>` so split by '/'
-	tmp := strings.Split(prestoHostResults.String(), "/")[1]
-	host := strings.TrimSuffix(tmp, "\n")
-	t.Logf("host found for presto from prep phase: %s", host)
-
-	// TODO: need to handle the case where TLS is disabled
-	queryCmd := exec.Command(
-		"kubectl",
-		"-n", rf.Namespace,
-		"exec",
-		host,
-		"-c", "presto",
-		"--",
+	execCmd := []string{
 		"presto-cli",
-		"--server", "https://presto:8080",
-		"--user", "root",
-		"--catalog", "prometheus",
-		"--schema", "default",
-		"--keystore-path", "/opt/presto/tls/keystore.pem",
+		"--server",
+		"https://presto:8080",
+		"--user",
+		"root",
+		"--catalog",
+		"prometheus",
+		"--schema",
+		"default",
+		"--keystore-path",
+		"/opt/presto/tls/keystore.pem",
 		"--execute",
 		"SELECT * FROM prometheus.default.up where timestamp > (NOW() - INTERVAL '10' second)",
-	)
+	}
+	options := testhelpers.NewExecOptions(pod.Name, pod.Namespace, prestoContainerName, false, execCmd)
 
-	var queryResults bytes.Buffer
-	queryCmd.Stderr = os.Stderr
-	queryCmd.Stdout = &queryResults
-
-	err = queryCmd.Run()
+	stdoutBuf, stderrBuf, err := testhelpers.ExecPodCommandWithOptions(rf.KubeConfig, rf.KubeClient, options)
 	require.Nil(t, err, "expected running the a presto query would produce no error")
-	require.Containsf(t, queryResults.String(), "endpoint=metrics", "expected the presto query output would contain `endpoint=metrics`")
+	require.Containsf(t, stdoutBuf.String(), "endpoint=metrics", "expected the presto query output would contain `endpoint=metrics`")
+	require.Len(t, stderrBuf.String(), 0, "expected that the stderr buffer would return nothing")
 }
