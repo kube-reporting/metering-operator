9f62b7a5447d3d7a2927addb038e92b4922158de
timflannagan1 <timflannagan@gmail.com>
2020-11-03 10:01:15 -0500

    test/e2e: Update the leader election ConfigMap const variable name

--- test/e2e/ensure_leader_election_configmap_is_created_test.go
+++ test/e2e/ensure_leader_election_configmap_is_created_test.go
@@ -9,15 +9,13 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// TODO: change this name once metering has migrated to using
-// the ansible-operator v1.x version.
-const leaderElectionConfigMapName = "metering-operator-lock"
+const leaderElectionConfigMapName = "metering-operator"
 
 // testLeaderElctionConfigMapIsCreated is reponsible for querying for the metering operator
 // leader election lock ConfigMap. Note: this implementation is subject to change based on the
-// ansible-operator v1 migration. Currently, a ConfigMap is created using the `POD_NAME` environment
-// variable exposed using the k8s downward API. In the ansible-operator 1.x world, leader election
-// is determined using controller runtime's implementation.
+// ansible-operator v1 migration. Before, a ConfigMap is created using the `POD_NAME` environment
+// variable exposed using the k8s downward API. Now, leader election is determined using
+// controller runtime's implementation.
 func testLeaderElectionConfigMapIsCreated(t *testing.T, rf *reportingframework.ReportingFramework) {
 	_, err := rf.KubeClient.CoreV1().ConfigMaps(rf.Namespace).Get(context.Background(), leaderElectionConfigMapName, metav1.GetOptions{})
 	require.NoError(t, err, "failed to query for the %s leader election ConfigMap", leaderElectionConfigMapName)
