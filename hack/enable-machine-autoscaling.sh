32e4c0c1a41f2bacaf5e42bb71e71f5d5d836097
timflannagan1 <timflannagan@gmail.com>
2020-11-05 10:51:34 -0500

    hack: Update the path to the machine autoscaling YAML manifests

--- hack/enable-machine-autoscaling.sh
+++ hack/enable-machine-autoscaling.sh
@@ -4,14 +4,15 @@ set -eoux pipefail
 
 : "${KUBECONFIG:?}"
 ROOT_DIR=$(dirname "${BASH_SOURCE[0]}")/..
-MANIFESTS_BASE_PATH="${ROOT_DIR}/olm_deploy/manifests/machines"
+MANIFESTS_BASE_PATH="${ROOT_DIR}/test/e2e/manifests/machines"
 
 if ! kubectl get clusterautoscalers default > /dev/null 2>&1; then
-    kubectl create -f ${MANIFESTS_BASE_PATH}/clusterautoscaler.yaml
+    kubectl create -f "${MANIFESTS_BASE_PATH}"/clusterautoscaler.yaml
 fi
 
 MACHINESETS=( $(oc -n openshift-machine-api get machinesets --no-headers | awk '{ print $1 }') )
 for machine in "${MACHINESETS[@]}"; do
-    export MACHINE_NAME=$(oc -n openshift-machine-api get machineset $machine -o jsonpath='{.metadata.name}')
-    envsubst < ${MANIFESTS_BASE_PATH}/machineautoscaler.yaml | kubectl apply -f -
+    MACHINE_NAME=$(oc -n openshift-machine-api get machineset "$machine" -o jsonpath='{.metadata.name}')
+    export MACHINE_NAME
+    envsubst < "${MANIFESTS_BASE_PATH}"/machineautoscaler.yaml | kubectl apply -f -
 done
